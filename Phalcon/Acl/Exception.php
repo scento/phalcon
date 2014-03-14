c13920be495f2f9dfdf0f72aef52f93ff243d492
Wenzel Pünter <wenzel@phelix.me>
2014-03-14 16:53:00 +0100

    ACL Exception restructure

--- Phalcon/Acl/Exception.php
+++ Phalcon/Acl/Exception.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
+/**
+ * ACL Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Acl;
 
-namespace Phalcon\Acl {
-
-	/**
-	 * Phalcon\Acl\Exception
-	 *
-	 * Class for exceptions thrown by Phalcon\Acl
-	 */
+/**
+ * Phalcon\Acl\Exception
+ *
+ * Class for exceptions thrown by Phalcon\Acl
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/acl/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

7be532274e81096158e47a74744852ccefc190a5
Wenzel Pünter <wenzel@phelix.me>
2014-03-14 16:55:35 +0100

    ACL Role implementation

--- Phalcon/Acl/Role.php
+++ Phalcon/Acl/Role.php
@@ -1,51 +1,101 @@
-<?php 
+<?php
+/**
+ * ACL Role
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Acl;
 
-namespace Phalcon\Acl {
+use \Phalcon\Acl\RoleInterface,
+	\Phalcon\Acl\Exception;
+
+/**
+ * Phalcon\Acl\Role
+ *
+ * This class defines role entity and its description
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/acl/role.c
+ */
+class Role implements RoleInterface
+{
+	/**
+	 * Name
+	 * 
+	 * @var string
+	 * @access protected
+	*/
+	protected $_name = null;
 
 	/**
-	 * Phalcon\Acl\Role
+	 * Description
+	 * 
+	 * @var string|null
+	 * @access protected
+	*/
+	protected $_description = null;
+
+	/**
+	 * \Phalcon\Acl\Role description
 	 *
-	 * This class defines role entity and its description
-	 *
+	 * @param string $name
+	 * @param string $description
 	 */
-	
-	class Role implements \Phalcon\Acl\RoleInterface {
+	public function __construct($name, $description = null)
+	{
+		if(is_string($name) === false)
+		{
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_name;
+		if(is_string($description) === false && is_null($description) === false)
+		{
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_description;
+		if($name === '*')
+		{
+			throw new Exception('Role name cannot be "*"');
+		}
 
-		/**
-		 * \Phalcon\Acl\Role description
-		 *
-		 * @param string $name
-		 * @param string $description
-		 */
-		public function __construct($name, $description=null){ }
+		$this->_name = $name;
 
+		if(is_null($description) === false)
+		{
+			$this->_description = $description;
+		}
+	}
 
-		/**
-		 * Returns the role name
-		 *
-		 * @return string
-		 */
-		public function getName(){ }
+	/**
+	 * Returns the role name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->_name;
+	}
 
+	/**
+	 * Returns role description
+	 *
+	 * @return string|null
+	 */
+	public function getDescription()
+	{
+		return $this->_description;
+	}
 
-		/**
-		 * Returns role description
-		 *
-		 * @return string
-		 */
-		public function getDescription(){ }
-
-
-		/**
-		 * Magic method __toString
-		 *
-		 * @return string
-		 */
-		public function __toString(){ }
-
+	/**
+	 * Magic method __toString
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->_name;
 	}
 }

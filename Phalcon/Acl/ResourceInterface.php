9ac56fd92b84094024a7196eb4e91e2ffb96f262
Wenzel Pünter <wenzel@phelix.me>
2014-03-14 16:54:41 +0100

    ACL ResourceInterface restructure

--- Phalcon/Acl/ResourceInterface.php
+++ Phalcon/Acl/ResourceInterface.php
@@ -1,44 +1,47 @@
-<?php 
+<?php
+/**
+ * ACL Resource Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Acl;
 
-namespace Phalcon\Acl {
+/**
+ * Phalcon\Acl\ResourceInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/acl/resourceinterface.c
+ */
+interface ResourceInterface
+{
+	/**
+	 * \Phalcon\Acl\ResourceInterface constructor
+	 *
+	 * @param string $name
+	 * @param string $description
+	 */
+	public function __construct($name, $description = null);
 
 	/**
-	 * Phalcon\Acl\ResourceInterface initializer
+	 * Returns the resource name
+	 *
+	 * @return string
 	 */
-	
-	interface ResourceInterface {
+	public function getName();
 
-		/**
-		 * \Phalcon\Acl\ResourceInterface constructor
-		 *
-		 * @param string $name
-		 * @param string $description
-		 */
-		public function __construct($name, $description=null);
+	/**
+	 * Returns resource description
+	 *
+	 * @return string|null
+	 */
+	public function getDescription();
 
-
-		/**
-		 * Returns the resource name
-		 *
-		 * @return string
-		 */
-		public function getName();
-
-
-		/**
-		 * Returns resource description
-		 *
-		 * @return string
-		 */
-		public function getDescription();
-
-
-		/**
-		 * Magic method __toString
-		 *
-		 * @return string
-		 */
-		public function __toString();
-
-	}
+	/**
+	 * Magic method __toString
+	 *
+	 * @return string
+	 */
+	public function __toString();
 }

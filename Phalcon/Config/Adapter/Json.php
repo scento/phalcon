67f1757ae5c01c9c638b588447b523adf6963acc
Wenzel Pünter <wenzel@phelix.me>
2014-03-15 21:16:45 +0100

    Config JSON Adapter implementation

--- Phalcon/Config/Adapter/Json.php
+++ Phalcon/Config/Adapter/Json.php
@@ -1,36 +1,71 @@
-<?php 
+<?php
+/**
+ * JSON Adapter
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Config\Adapter;
 
-namespace Phalcon\Config\Adapter {
-
-	/**
-	 * Phalcon\Config\Adapter\Json
-	 *
-	 * Reads JSON files and converts them to Phalcon\Config objects.
-	 *
-	 * Given the following configuration file:
-	 *
-	 *<code>
-	 *{"phalcon":{"baseuri":"\/phalcon\/"},"models":{"metadata":"memory"}}
-	 *</code>
-	 *
-	 * You can read it as follows:
-	 *
-	 *<code>
-	 *	$config = new Phalcon\Config\Adapter\Json("path/config.json");
-	 *	echo $config->phalcon->baseuri;
-	 *	echo $config->models->metadata;
-	 *</code>
-	 *
-	 */
+use \Countable,
+	\ArrayAccess,
+	\Phalcon\Config,
+	\Phalcon\Config\Exception;
 	
-	class Json extends \Phalcon\Config implements \Countable, \ArrayAccess {
 
-		/**
-		 * \Phalcon\Config\Adapter\Json constructor
-		 *
-		 * @param string $filePath
-		 */
-		public function __construct($filePath){ }
+/**
+ * Phalcon\Config\Adapter\Json
+ *
+ * Reads JSON files and converts them to Phalcon\Config objects.
+ *
+ * Given the following configuration file:
+ *
+ *<code>
+ *{"phalcon":{"baseuri":"\/phalcon\/"},"models":{"metadata":"memory"}}
+ *</code>
+ *
+ * You can read it as follows:
+ *
+ *<code>
+ *	$config = new Phalcon\Config\Adapter\Json("path/config.json");
+ *	echo $config->phalcon->baseuri;
+ *	echo $config->models->metadata;
+ *</code>
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/config/adapter/json.c
+ */
+class Json extends Config implements Countable, ArrayAccess
+{
+	/**
+	 * \Phalcon\Config\Adapter\Json constructor
+	 *
+	 * @param string $filePath
+	 * @throws Exception
+	 */
+	public function __construct($filePath)
+	{
+		if(is_string($filePath) === false)
+		{
+			throw new Exception('Invalid parameter type.');
+		}
 
+		$contents = file_get_contents($filePath);
+		if(is_string($contents) === true)
+		{
+			$array = json_decode($contents, true);
+
+			if(json_last_error() !== \JSON_ERROR_NONE)
+			{
+				throw new Exception('Invalid json file.');
+			}
+
+			parent::__construct($array);
+
+		} else {
+			throw new Exception('Unable to read json file.');
+		}
 	}
 }

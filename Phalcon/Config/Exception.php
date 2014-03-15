af308e84bb98eaf7131783b86181c388f38bdcd9
Wenzel Pünter <wenzel@phelix.me>
2014-03-15 13:24:07 +0100

    Config Exception restructure

--- Phalcon/Config/Exception.php
+++ Phalcon/Config/Exception.php
@@ -1,14 +1,20 @@
-<?php 
+<?php
+/**
+ * ACL
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Config;
 
-namespace Phalcon\Config {
-
-	/**
-	 * Phalcon\Config\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Config will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
+/**
+ * Phalcon\Config\Exception
+ *
+ * Exceptions thrown in Phalcon\Config will use this class
+ *
+ */
+class Exception extends \Phalcon\Exception
+{
 }

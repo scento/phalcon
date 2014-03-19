9a1c3024a0425927a5865496b15e36594add20f5
Wenzel Pünter <wenzel@phelix.me>
2014-03-19 12:30:15 +0100

    Annotations Exception restructure

--- Phalcon/Annotations/Exception.php
+++ Phalcon/Annotations/Exception.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
+/**
+ * ACL Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Annotations;
 
-namespace Phalcon\Annotations {
+/**
+ * Phalcon\Annotations\Exception
+ *
+ * Exceptions thrown in Phalcon\Annotations will use this class
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/annotations/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Annotations\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Annotations will use this class
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

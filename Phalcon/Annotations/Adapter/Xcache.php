9790413e5f49b5fd8e9e5f99320e4c8f1a919993
Wenzel Pünter <wenzel@phelix.me>
2014-03-26 19:13:39 +0100

    Annotations XCache Adapter implementation

--- Phalcon/Annotations/Adapter/Xcache.php
+++ Phalcon/Annotations/Adapter/Xcache.php
@@ -1,35 +1,67 @@
-<?php 
+<?php
+/**
+ * Annotations XCache Adapter
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Annotations\Adapter;
 
-namespace Phalcon\Annotations\Adapter {
+use \Phalcon\Annotations\AdapterInterface,
+	\Phalcon\Annotations\Adapter,
+	\Phalcon\Annotations\Exception,
+	\Phalcon\Annotations\Reflection;
+
+/**
+ * Phalcon\Annotations\Adapter\Xcache
+ *
+ * Stores the parsed annotations to XCache. This adapter is suitable for production
+ *
+ *<code>
+ * $annotations = new \Phalcon\Annotations\Adapter\Xcache();
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/annotations/adapter/xcache.c
+ */
+class Xcache extends Adapter implements AdapterInterface
+{
+	/**
+	 * Reads parsed annotations from XCache
+	 *
+	 * @param string $key
+	 * @return \Phalcon\Annotations\Reflection|null
+	 * @throws Exception
+	 */
+	public function read($key)
+	{
+		if(is_string($key) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
+
+		$serialized = xcache_get(strtolower('_PHAN'.$key));
+		if(is_string($serialized) === true) {
+			$unserialized = unserialize($serialized);
+			return (is_object($unserialized) === true ? $unserialized : null);
+		}
+	}
 
 	/**
-	 * Phalcon\Annotations\Adapter\Xcache
+	 * Writes parsed annotations to XCache
 	 *
-	 * Stores the parsed annotations to XCache. This adapter is suitable for production
-	 *
-	 *<code>
-	 * $annotations = new \Phalcon\Annotations\Adapter\Xcache();
-	 *</code>
+	 * @param string $key
+	 * @param \Phalcon\Annotations\Reflection $data
+	 * @throws Exception
 	 */
-	
-	class Xcache extends \Phalcon\Annotations\Adapter implements \Phalcon\Annotations\AdapterInterface {
+	public function write($key, Reflection $data)
+	{
+		if(is_string($key) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Reads parsed annotations from XCache
-		 *
-		 * @param string $key
-		 * @return \Phalcon\Annotations\Reflection
-		 */
-		public function read($key){ }
-
-
-		/**
-		 * Writes parsed annotations to XCache
-		 *
-		 * @param string $key
-		 * @param \Phalcon\Annotations\Reflection $data
-		 */
-		public function write($key, $data){ }
-
+		$serialized = serialize($data);
+		xcache_set(strtolower('_PHAN'.$key), $serialized);
 	}
 }

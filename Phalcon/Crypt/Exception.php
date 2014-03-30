a1e6ad6752fd4760b4ff7b6598cabe30d87ce7a0
Wenzel Pünter <wenzel@phelix.me>
2014-03-30 12:28:37 +0200

    Crypt Exception restructure

--- Phalcon/Crypt/Exception.php
+++ Phalcon/Crypt/Exception.php
@@ -1,14 +1,20 @@
-<?php 
+<?php
+/**
+ * Crypt Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon\Crypt;
 
-namespace Phalcon\Crypt {
-
-	/**
-	 * Phalcon\Escaper\Crypt
-	 *
-	 * Exceptions thrown in Phalcon\Crypt use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
+/**
+ * Phalcon\Crypt\Exception
+ *
+ * Exceptions thrown in Phalcon\Crypt use this class
+ *
+ */
+class Exception extends \Phalcon\Exception
+{
 }

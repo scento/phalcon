a7dae05b5891afd18020e836bce31b41a3fc1d0c
Wenzel Pünter <wenzel@phelix.me>
2014-04-03 20:38:46 +0200

    Escaper interface restructure

--- Phalcon/EscaperInterface.php
+++ Phalcon/EscaperInterface.php
@@ -1,80 +1,79 @@
-<?php 
+<?php
+/**
+ * Escaper Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon;
 
-namespace Phalcon {
+/**
+ * Phalcon\EscaperInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/escaperinterface.c
+ */
+interface EscaperInterface
+{
+	/**
+	 * Sets the encoding to be used by the escaper
+	 *
+	 * @param string $encoding
+	 */
+	public function setEncoding($encoding);
 
 	/**
-	 * Phalcon\EscaperInterface initializer
+	 * Returns the internal encoding used by the escaper
+	 *
+	 * @return string
 	 */
-	
-	interface EscaperInterface {
+	public function getEncoding();
 
-		/**
-		 * Sets the encoding to be used by the escaper
-		 *
-		 * @param string $encoding
-		 */
-		public function setEncoding($encoding);
+	/**
+	 * Sets the HTML quoting type for htmlspecialchars
+	 *
+	 * @param int $quoteType
+	 */
+	public function setHtmlQuoteType($quoteType);
 
+	/**
+	 * Escapes a HTML string
+	 *
+	 * @param string $text
+	 * @return string
+	 */
+	public function escapeHtml($text);
 
-		/**
-		 * Returns the internal encoding used by the escaper
-		 *
-		 * @return string
-		 */
-		public function getEncoding();
+	/**
+	 * Escapes a HTML attribute string
+	 *
+	 * @param string $text
+	 * @return string
+	 */
+	public function escapeHtmlAttr($text);
 
+	/**
+	 * Escape CSS strings by replacing non-alphanumeric chars by their hexadecimal representation
+	 *
+	 * @param string $css
+	 * @return string
+	 */
+	public function escapeCss($css);
 
-		/**
-		 * Sets the HTML quoting type for htmlspecialchars
-		 *
-		 * @param int $quoteType
-		 */
-		public function setHtmlQuoteType($quoteType);
+	/**
+	 * Escape Javascript strings by replacing non-alphanumeric chars by their hexadecimal representation
+	 *
+	 * @param string $js
+	 * @return string
+	 */
+	public function escapeJs($js);
 
-
-		/**
-		 * Escapes a HTML string
-		 *
-		 * @param string $text
-		 * @return string
-		 */
-		public function escapeHtml($text);
-
-
-		/**
-		 * Escapes a HTML attribute string
-		 *
-		 * @param string $text
-		 * @return string
-		 */
-		public function escapeHtmlAttr($text);
-
-
-		/**
-		 * Escape CSS strings by replacing non-alphanumeric chars by their hexadecimal representation
-		 *
-		 * @param string $css
-		 * @return string
-		 */
-		public function escapeCss($css);
-
-
-		/**
-		 * Escape Javascript strings by replacing non-alphanumeric chars by their hexadecimal representation
-		 *
-		 * @param string $js
-		 * @return string
-		 */
-		public function escapeJs($js);
-
-
-		/**
-		 * Escapes a URL. Internally uses rawurlencode
-		 *
-		 * @param string $url
-		 * @return string
-		 */
-		public function escapeUrl($url);
-
-	}
+	/**
+	 * Escapes a URL. Internally uses rawurlencode
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	public function escapeUrl($url);
 }

c6ecc1156f8e56247dc0181ba189872840f3e83b
Wenzel Pünter <wenzel@phelix.me>
2014-04-04 15:45:11 +0200

    Exception restructure

--- Phalcon/Exception.php
+++ Phalcon/Exception.php
@@ -1,13 +1,20 @@
-<?php 
+<?php
+/**
+ * Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon;
 
-namespace Phalcon {
-
-	/**
-	 * Phalcon\Exception
-	 *
-	 * All framework exceptions should use or extend this exception
-	 */
+/**
+ * Phalcon\Exception
+ *
+ * All framework exceptions should use or extend this exception
+ */
+class Exception extends \Exception
+{
 	
-	class Exception extends \Exception {
-	}
 }

7c498b4865c0adf52ef8e59f48172ec2c2307299
Wenzel Pünter <wenzel@phelix.me>
2014-04-04 15:42:38 +0200

    Filter interface restructure

--- Phalcon/FilterInterface.php
+++ Phalcon/FilterInterface.php
@@ -1,39 +1,41 @@
-<?php 
+<?php
+/**
+ * Filter Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 0.1
+ * @package Phalcon
+*/
+namespace Phalcon;
 
-namespace Phalcon {
+/**
+ * Phalcon\FilterInterface initializer
+ */
+interface FilterInterface
+{
+	/**
+	 * Adds a user-defined filter
+	 *
+	 * @param string $name
+	 * @param callable $handler
+	 * @return \Phalcon\FilterInterface
+	 */
+	public function add($name, $handler);
 
 	/**
-	 * Phalcon\FilterInterface initializer
+	 * Sanizites a value with a specified single or set of filters
+	 *
+	 * @param  mixed $value
+	 * @param  mixed $filters
+	 * @return mixed
 	 */
-	
-	interface FilterInterface {
+	public function sanitize($value, $filters);
 
-		/**
-		 * Adds a user-defined filter
-		 *
-		 * @param string $name
-		 * @param callable $handler
-		 * @return \Phalcon\FilterInterface
-		 */
-		public function add($name, $handler);
-
-
-		/**
-		 * Sanizites a value with a specified single or set of filters
-		 *
-		 * @param  mixed $value
-		 * @param  mixed $filters
-		 * @return mixed
-		 */
-		public function sanitize($value, $filters);
-
-
-		/**
-		 * Return the user-defined filters in the instance
-		 *
-		 * @return object[]
-		 */
-		public function getFilters();
-
-	}
+	/**
+	 * Return the user-defined filters in the instance
+	 *
+	 * @return object[]
+	 */
+	public function getFilters();
 }

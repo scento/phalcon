19156c6d3e353bc5f2f87012df0a9f9c08676352
Wenzel Pünter <wenzel@phelix.me>
2014-04-06 13:52:14 +0200

    Filter Exception restructure

--- Phalcon/Filter/Exception.php
+++ Phalcon/Filter/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Filter Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Filter;
 
-namespace Phalcon\Filter {
-
-	/**
-	 * Phalcon\Filter\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Filter will use this class
-	 *
-	 */
+/**
+ * Phalcon\Filter\Exception
+ *
+ * Exceptions thrown in Phalcon\Filter will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/filter/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

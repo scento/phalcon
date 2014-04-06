a801cc64a1ab76bb0bf858b2db6a6a3c3d651b73
Wenzel Pünter <wenzel@phelix.me>
2014-04-06 13:53:17 +0200

    User Filter Interface restructure

--- Phalcon/Filter/UserFilterInterface.php
+++ Phalcon/Filter/UserFilterInterface.php
@@ -1,20 +1,26 @@
-<?php 
+<?php
+/**
+ * User Filter Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Filter;
 
-namespace Phalcon\Filter {
-
+/**
+ * Phalcon\Filter\UserFilterInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/filter/userfilterinterface.c
+ */
+interface UserFilterInterface
+{
 	/**
-	 * Phalcon\Filter\UserFilterInterface initializer
+	 * Filters a value
+	 *
+	 * @param mixed $value
+	 * @return mixed
 	 */
-	
-	interface UserFilterInterface {
-
-		/**
-		 * Filters a value
-		 *
-		 * @param mixed $value
-		 * @return mixed
-		 */
-		public function filter($value);
-
-	}
+	public function filter($value);
 }

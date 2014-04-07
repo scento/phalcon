bd38c4fa0846e3761d4b50ce370e971889263d25
Wenzel Pünter <wenzel@phelix.me>
2014-04-07 18:20:09 +0200

    Logger class restructure

--- Phalcon/Logger.php
+++ Phalcon/Logger.php
@@ -1,42 +1,99 @@
-<?php 
+<?php
+/**
+ * Logger
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon;
 
-namespace Phalcon {
+/**
+ * Phalcon\Logger
+ *
+ * Phalcon\Logger is a component whose purpose is create logs using
+ * different backends via adapters, generating options, formats and filters
+ * also implementing transactions.
+ *
+ *<code>
+ *	$logger = new Phalcon\Logger\Adapter\File("app/logs/test.log");
+ *	$logger->log("This is a message");
+ *	$logger->log("This is an error", Phalcon\Logger::ERROR);
+ *	$logger->error("This is another error");
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/logger.c
+ */
+abstract class Logger
+{
+	/**
+	 * Special
+	 * 
+	 * @var int
+	*/
+	const SPECIAL = 9;
 
 	/**
-	 * Phalcon\Logger
-	 *
-	 * Phalcon\Logger is a component whose purpose is create logs using
-	 * different backends via adapters, generating options, formats and filters
-	 * also implementing transactions.
-	 *
-	 *<code>
-	 *	$logger = new Phalcon\Logger\Adapter\File("app/logs/test.log");
-	 *	$logger->log("This is a message");
-	 *	$logger->log("This is an error", Phalcon\Logger::ERROR);
-	 *	$logger->error("This is another error");
-	 *</code>
-	 */
-	
-	abstract class Logger {
+	 * Custom
+	 * 
+	 * @var int
+	*/
+	const CUSTOM = 8;
 
-		const SPECIAL = 9;
+	/**
+	 * Debug
+	 * 
+	 * @var int
+	*/
+	const DEBUG = 7;
 
-		const CUSTOM = 8;
+	/**
+	 * Info
+	 * 
+	 * @var int
+	*/
+	const INFO = 6;
 
-		const DEBUG = 7;
+	/**
+	 * Notice
+	 * 
+	 * @var int
+	*/
+	const NOTICE = 5;
 
-		const INFO = 6;
+	/**
+	 * Warning
+	 * 
+	 * @var int
+	*/
+	const WARNING = 4;
 
-		const NOTICE = 5;
+	/**
+	 * Error
+	 * 
+	 * @var int
+	*/
+	const ERROR = 3;
 
-		const WARNING = 4;
+	/**
+	 * Alert
+	 * 
+	 * @var int
+	*/
+	const ALERT = 2;
 
-		const ERROR = 3;
+	/**
+	 * Critical
+	 * 
+	 * @var int
+	*/
+	const CRITICAL = 1;
 
-		const ALERT = 2;
-
-		const CRITICAL = 1;
-
-		const EMERGENCE = 0;
-	}
+	/**
+	 * Emergence
+	 * 
+	 * @var int
+	*/
+	const EMERGENCE = 0;
 }

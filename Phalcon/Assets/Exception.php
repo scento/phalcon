b682a38aee0e76f480be74779b957e432efba373
Wenzel Pünter <wenzel@phelix.me>
2014-04-11 14:35:39 +0200

    Assets Exception restructure

--- Phalcon/Assets/Exception.php
+++ Phalcon/Assets/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Assets Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Assets;
 
-namespace Phalcon\Assets {
+/**
+ * Phalcon\DI\Exception
+ *
+ * Exceptions thrown in Phalcon\Assets will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/master/ext/assets/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\DI\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Assets will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

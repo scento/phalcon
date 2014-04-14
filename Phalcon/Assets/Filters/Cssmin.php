0f185981ae19f63e31e0fc9ff5d3a7882823b94c
Wenzel Pünter <wenzel@phelix.me>
2014-04-14 13:55:13 +0200

    CssMin Assets filter implementation

--- Phalcon/Assets/Filters/Cssmin.php
+++ Phalcon/Assets/Filters/Cssmin.php
@@ -1,24 +1,64 @@
-<?php 
+<?php
+/**
+ * Cssmin Filter
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Assets\Filters;
 
-namespace Phalcon\Assets\Filters {
+use \Phalcon\Assets\Exception,
+	\Phalcon\Assets\Filters\Helper\CssMin;
+
+/**
+ * Phalcon\Assets\Filters\Cssmin
+ *
+ * Minify the css - removes comments
+ * removes newlines and line feeds keeping
+ * removes last semicolon from last property
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/assets/filters/cssmin.c
+ */
+class Cssmin
+{
+	/**
+	 * Filters the content using CSSMIN
+	 *
+	 * @param string $content
+	 * @return string|null
+	 */
+	public function filter($content)
+	{
+		try {
+			return self::cssmin($content);
+		} catch(Exception $e) {
+			return null;
+		}
+	}
 
 	/**
-	 * Phalcon\Assets\Filters\Cssmin
-	 *
-	 * Minify the css - removes comments
-	 * removes newlines and line feeds keeping
-	 * removes last semicolon from last property
-	 */
-	
-	class Cssmin {
+	 * Cssmin
+	 * 
+	 * @param string $content
+	 * @return string
+	 * @throws Exception
+	*/
+	private static function cssmin($content) {
+		if(is_string($content) === false) {
+			throw new Exception('Style must be a string');
+		}
 
-		/**
-		 * Filters the content using CSSMIN
-		 *
-		 * @param string $content
-		 * @return $content
-		 */
-		public function filter($content){ }
-
+		try {
+			return CssMin::minify($content);
+		} catch(\Exception $e) {
+			if(is_string($e) === true) {
+				throw new Exception($e);
+			} else {
+				throw new Exception('Unknown error');
+			}
+		}
 	}
 }

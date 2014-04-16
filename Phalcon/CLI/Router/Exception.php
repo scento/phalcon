00502ab194c89badbeebc4c8e212db6533b65aa4
Wenzel Pünter <wenzel@phelix.me>
2014-04-16 14:59:35 +0200

    CLI Router Exception restructure

--- Phalcon/CLI/Router/Exception.php
+++ Phalcon/CLI/Router/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Router Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\CLI\Router;
 
-namespace Phalcon\CLI\Router {
-
-	/**
-	 * Phalcon\CLI\Router\Exception
-	 *
-	 * Exceptions thrown in Phalcon\CLI\Router will use this class
-	 *
-	 */
+/**
+ * Phalcon\CLI\Router\Exception
+ *
+ * Exceptions thrown in Phalcon\CLI\Router will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/cli/router/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

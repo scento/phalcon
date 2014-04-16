3f7f9d4318213cbef00fecc34ca7a43f95d3e90e
Wenzel Pünter <wenzel@phelix.me>
2014-04-16 14:58:44 +0200

    CLI Dispatcher Exception restructure

--- Phalcon/CLI/Dispatcher/Exception.php
+++ Phalcon/CLI/Dispatcher/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Dispatcher Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\CLI\Dispatcher;
 
-namespace Phalcon\CLI\Dispatcher {
-
-	/**
-	 * Phalcon\CLI\Dispatcher\Exception
-	 *
-	 * Exceptions thrown in Phalcon\CLI\Dispatcher will use this class
-	 *
-	 */
+/**
+ * Phalcon\CLI\Dispatcher\Exception
+ *
+ * Exceptions thrown in Phalcon\CLI\Dispatcher will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/cli/dispatcher/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

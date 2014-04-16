037648909d18665b5df7e8e03debc3446a94e897
Wenzel Pünter <wenzel@phelix.me>
2014-04-16 14:57:35 +0200

    CLI Console Exception restructure

--- Phalcon/CLI/Console/Exception.php
+++ Phalcon/CLI/Console/Exception.php
@@ -9,14 +9,14 @@
 */
 namespace Phalcon\CLI\Console;
 
-	/**
-	 * Phalcon\CLI\Console\Exception
-	 *
-	 * Exceptions thrown in Phalcon\CLI\Console will use this class
-	 *
-	 * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/cli/console/exception.c
-	 */
-	class Exception extends \Phalcon\Exception
-	{
+/**
+ * Phalcon\CLI\Console\Exception
+ *
+ * Exceptions thrown in Phalcon\CLI\Console will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/cli/console/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	}
+}

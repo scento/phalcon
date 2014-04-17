b6a97a9b27e4d34fb6b7eb75d68c7e2da69fe550
Wenzel Pünter <wenzel@phelix.me>
2014-04-17 09:11:12 +0200

    CLI Task implementation

--- Phalcon/CLI/Task.php
+++ Phalcon/CLI/Task.php
@@ -1,42 +1,56 @@
-<?php 
+<?php
+/**
+ * CLI Task
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\CLI;
 
-namespace Phalcon\CLI {
+use \Phalcon\DI\Injectable,
+	\Phalcon\Events\EventsAwareInterface,
+	\Phalcon\DI\InjectionAwareInterface;
 
+/**
+ * Phalcon\CLI\Task
+ *
+ * Every command-line task should extend this class that encapsulates all the task functionality
+ *
+ * A task can be used to run "tasks" such as migrations, cronjobs, unit-tests, or anything that you want.
+ * The Task class should at least have a "mainAction" method
+ *
+ *<code>
+ *
+ *class HelloTask extends \Phalcon\CLI\Task
+ *{
+ *
+ *  //This action will be executed by default
+ *  public function mainAction()
+ *  {
+ *
+ *  }
+ *
+ *  public function findAction()
+ *  {
+ *
+ *  }
+ *
+ *}
+ *
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/cli/task.c
+ */
+class Task extends Injectable implements EventsAwareInterface, InjectionAwareInterface
+{
 	/**
-	 * Phalcon\CLI\Task
-	 *
-	 * Every command-line task should extend this class that encapsulates all the task functionality
-	 *
-	 * A task can be used to run "tasks" such as migrations, cronjobs, unit-tests, or anything that you want.
-	 * The Task class should at least have a "mainAction" method
-	 *
-	 *<code>
-	 *
-	 *class HelloTask extends \Phalcon\CLI\Task
-	 *{
-	 *
-	 *  //This action will be executed by default
-	 *  public function mainAction()
-	 *  {
-	 *
-	 *  }
-	 *
-	 *  public function findAction()
-	 *  {
-	 *
-	 *  }
-	 *
-	 *}
-	 *
-	 *</code>
+	 * \Phalcon\CLI\Task constructor
 	 */
-	
-	class Task extends \Phalcon\DI\Injectable implements \Phalcon\Events\EventsAwareInterface, \Phalcon\DI\InjectionAwareInterface {
-
-		/**
-		 * \Phalcon_CLI_Task constructor
-		 */
-		final public function __construct(){ }
-
+	final public function __construct()
+	{
+		//Marvel at the original code...
 	}
 }

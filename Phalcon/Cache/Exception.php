56f7e29d24bf88cadac69d8ac1e60c18b0047979
Wenzel Pünter <wenzel@phelix.me>
2014-04-18 11:54:05 +0200

    Cache Exception restructure

--- Phalcon/Cache/Exception.php
+++ Phalcon/Cache/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Cache Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Cache;
 
-namespace Phalcon\Cache {
+/**
+ * Phalcon\Cache\Exception
+ *
+ * Exceptions thrown in Phalcon\Cache will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/cache/exception.c
+ */	
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Cache\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Cache will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

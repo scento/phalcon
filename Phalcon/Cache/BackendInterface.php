0d9989e2ce26891f210aea7767793bd770282809
Wenzel Pünter <wenzel@phelix.me>
2014-04-18 11:53:10 +0200

    Cache Backend Interface restructure

--- Phalcon/Cache/BackendInterface.php
+++ Phalcon/Cache/BackendInterface.php
@@ -1,126 +1,120 @@
-<?php 
+<?php
+/**
+ * Backend Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Cache;
 
-namespace Phalcon\Cache {
+/**
+ * Phalcon\Cache\BackendInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/cache/backendinterface.c
+ */
+interface BackendInterface
+{
+	/**
+	 * Starts a cache. The $keyname allows to identify the created fragment
+	 *
+	 * @param int|string $keyName
+	 * @param int|null $lifetime
+	 * @return mixed
+	 */
+	public function start($keyName, $lifetime = null);
 
 	/**
-	 * Phalcon\Cache\BackendInterface initializer
+	 * Stops the frontend without store any cached content
+	 *
+	 * @param boolean|null $stopBuffer
 	 */
-	
-	interface BackendInterface {
+	public function stop($stopBuffer = null);
 
-		/**
-		 * Starts a cache. The $keyname allows to identify the created fragment
-		 *
-		 * @param int|string $keyName
-		 * @param   long $lifetime
-		 * @return  mixed
-		 */
-		public function start($keyName, $lifetime=null);
+	/**
+	 * Returns front-end instance adapter related to the back-end
+	 *
+	 * @return mixed
+	 */
+	public function getFrontend();
 
+	/**
+	 * Returns the backend options
+	 *
+	 * @return array
+	 */
+	public function getOptions();
 
-		/**
-		 * Stops the frontend without store any cached content
-		 *
-		 * @param boolean $stopBuffer
-		 */
-		public function stop($stopBuffer=null);
+	/**
+	 * Checks whether the last cache is fresh or cached
+	 *
+	 * @return boolean
+	 */
+	public function isFresh();
 
+	/**
+	 * Checks whether the cache has starting buffering or not
+	 *
+	 * @return boolean
+	 */
+	public function isStarted();
 
-		/**
-		 * Returns front-end instance adapter related to the back-end
-		 *
-		 * @return mixed
-		 */
-		public function getFrontend();
+	/**
+	 * Sets the last key used in the cache
+	 *
+	 * @param string $lastKey
+	 */
+	public function setLastKey($lastKey);
 
+	/**
+	 * Gets the last key stored by the cache
+	 *
+	 * @return string
+	 */
+	public function getLastKey();
 
-		/**
-		 * Returns the backend options
-		 *
-		 * @return array
-		 */
-		public function getOptions();
+	/**
+	 * Returns a cached content
+	 *
+	 * @param int|string $keyName
+	 * @param int|null $lifetime
+	 * @return mixed
+	 */
+	public function get($keyName, $lifetime = null);
 
+	/**
+	 * Stores cached content into the file backend and stops the frontend
+	 *
+	 * @param int|string|null $keyName
+	 * @param string|null $content
+	 * @param int|null $lifetime
+	 * @param boolean|null $stopBuffer
+	 */
+	public function save($keyName = null, $content = null, $lifetime = null, $stopBuffer = null);
 
-		/**
-		 * Checks whether the last cache is fresh or cached
-		 *
-		 * @return boolean
-		 */
-		public function isFresh();
+	/**
+	 * Deletes a value from the cache by its key
+	 *
+	 * @param int|string $keyName
+	 * @return boolean
+	 */
+	public function delete($keyName);
 
+	/**
+	 * Query the existing cached keys
+	 *
+	 * @param string|null $prefix
+	 * @return array
+	 */
+	public function queryKeys($prefix = null);
 
-		/**
-		 * Checks whether the cache has starting buffering or not
-		 *
-		 * @return boolean
-		 */
-		public function isStarted();
-
-
-		/**
-		 * Sets the last key used in the cache
-		 *
-		 * @param string $lastKey
-		 */
-		public function setLastKey($lastKey);
-
-
-		/**
-		 * Gets the last key stored by the cache
-		 *
-		 * @return string
-		 */
-		public function getLastKey();
-
-
-		/**
-		 * Returns a cached content
-		 *
-		 * @param int|string $keyName
-		 * @param   long $lifetime
-		 * @return  mixed
-		 */
-		public function get($keyName, $lifetime=null);
-
-
-		/**
-		 * Stores cached content into the file backend and stops the frontend
-		 *
-		 * @param int|string $keyName
-		 * @param string $content
-		 * @param long $lifetime
-		 * @param boolean $stopBuffer
-		 */
-		public function save($keyName=null, $content=null, $lifetime=null, $stopBuffer=null);
-
-
-		/**
-		 * Deletes a value from the cache by its key
-		 *
-		 * @param int|string $keyName
-		 * @return boolean
-		 */
-		public function delete($keyName);
-
-
-		/**
-		 * Query the existing cached keys
-		 *
-		 * @param string $prefix
-		 * @return array
-		 */
-		public function queryKeys($prefix=null);
-
-
-		/**
-		 * Checks if cache exists and it hasn't expired
-		 *
-		 * @param  string $keyName
-		 * @param  long $lifetime
-		 * @return boolean
-		 */
-		public function exists($keyName=null, $lifetime=null);
-
-	}
+	/**
+	 * Checks if cache exists and it hasn't expired
+	 *
+	 * @param string|null $keyName
+	 * @param int|null $lifetime
+	 * @return boolean
+	 */
+	public function exists($keyName = null, $lifetime = null);
 }

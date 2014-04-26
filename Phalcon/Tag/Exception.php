dc9efb8dc5f48af040e9974d6a8147d65fe82e59
Wenzel Pünter <wenzel@phelix.me>
2014-04-26 19:58:06 +0200

    Tag Exception restructure

--- Phalcon/Tag/Exception.php
+++ Phalcon/Tag/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Tag Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Tag;
 
-namespace Phalcon\Tag {
-
-	/**
-	 * Phalcon\Tag\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Tag will use this class
-	 *
-	 */
+/**
+ * Phalcon\Tag\Exception
+ *
+ * Exceptions thrown in Phalcon\Tag will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/tag/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

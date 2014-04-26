165e0a0c9a4df3946c4524cb42c5243e34d7f92d
Wenzel Pünter <wenzel@phelix.me>
2014-04-26 15:55:23 +0200

    InjectionAwareInterface restructure

--- Phalcon/DI/InjectionAwareInterface.php
+++ Phalcon/DI/InjectionAwareInterface.php
@@ -1,27 +1,32 @@
-<?php 
+<?php
+/**
+ * Injection Aware Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\DI;
 
-namespace Phalcon\DI {
+/**
+ * Phalcon\DI\InjectionAwareInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/di/injectionawareinterface.c
+ */
+interface InjectionAwareInterface
+{
+	/**
+	 * Sets the dependency injector
+	 *
+	 * @param \Phalcon\DiInterface $dependencyInjector
+	 */
+	public function setDI($dependencyInjector);
 
 	/**
-	 * Phalcon\DI\InjectionAwareInterface initializer
+	 * Returns the internal dependency injector
+	 *
+	 * @return \Phalcon\DiInterface
 	 */
-	
-	interface InjectionAwareInterface {
-
-		/**
-		 * Sets the dependency injector
-		 *
-		 * @param \Phalcon\DiInterface $dependencyInjector
-		 */
-		public function setDI($dependencyInjector);
-
-
-		/**
-		 * Returns the internal dependency injector
-		 *
-		 * @return \Phalcon\DiInterface
-		 */
-		public function getDI();
-
-	}
+	public function getDI();
 }

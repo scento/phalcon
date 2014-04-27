76252dcce458e3acd4535b935ac060def2407c04
Wenzel Pünter <wenzel@phelix.me>
2014-04-27 13:21:34 +0200

    Flash Exception restructure

--- Phalcon/Flash/Exception.php
+++ Phalcon/Flash/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Flash Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Flash;
 
-namespace Phalcon\Flash {
+/**
+ * Phalcon\Flash\Exception
+ *
+ * Exceptions thrown in Phalcon\Flash will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/flash/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Flash\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Flash will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

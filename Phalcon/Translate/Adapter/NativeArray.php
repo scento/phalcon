1c200ff7cc0ad6f28dc63dd3a1c88e0e2d429b20
Wenzel Pünter <wenzel@phelix.me>
2014-04-27 17:07:45 +0200

    Native Array Translation Adapter implementation

--- Phalcon/Translate/Adapter/NativeArray.php
+++ Phalcon/Translate/Adapter/NativeArray.php
@@ -1,43 +1,106 @@
-<?php 
+<?php
+/**
+ * Native Array Translation Adapter
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Translate\Adapter;
 
-namespace Phalcon\Translate\Adapter {
+use \Phalcon\Translate\Adapter,
+	\ArrayAccess,
+	\Phalcon\Translate\AdapterInterface,
+	\Phalcon\Translate\Exception;
+	
+/**
+ * Phalcon\Translate\Adapter\NativeArray
+ *
+ * Allows to define translation lists using PHP arrays
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/translate/adapter/nativearray.c
+ */
+class NativeArray extends Adapter implements \ArrayAccess, AdapterInterface
+{
+	/**
+	 * Translate
+	 * 
+	 * @var null|array
+	 * @access protected
+	*/
+	protected $_translate;
 
 	/**
-	 * Phalcon\Translate\Adapter\NativeArray
+	 * \Phalcon\Translate\Adapter\NativeArray constructor
 	 *
-	 * Allows to define translation lists using PHP arrays
-	 *
+	 * @param array $options
+	 * @throws Exception
 	 */
-	
-	class NativeArray extends \Phalcon\Translate\Adapter implements \ArrayAccess, \Phalcon\Translate\AdapterInterface {
+	public function __construct($options)
+	{
+		if(is_array($options) === false) {
+			throw new Exception('Invalid options');
+		}
 
-		protected $_translate;
+		if(isset($options['content']) === false) {
+			throw new Exception('Translation content was not provided');
+		}
 
-		/**
-		 * \Phalcon\Translate\Adapter\NativeArray constructor
-		 *
-		 * @param array $options
-		 */
-		public function __construct($options){ }
+		if(is_array($options['content']) === false) {
+			throw new Exception('Translation data must be an array');
+		}
 
+		$this->_translate = $options['content'];
+	}
 
-		/**
-		 * Returns the translation related to the given key
-		 *
-		 * @param string $index
-		 * @param array $placeholders
-		 * @return string
-		 */
-		public function query($index, $placeholders=null){ }
+	/**
+	 * Returns the translation related to the given key
+	 *
+	 * @param string $index
+	 * @param array|null $placeholders
+	 * @return string
+	 * @throws Exception
+	 */
+	public function query($index, $placeholders = null)
+	{
+		if(is_string($index) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		if(is_array($placeholders) === false &&
+			is_null($placeholders) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Check whether is defined a translation key in the internal array
-		 *
-		 * @param 	string $index
-		 * @return bool
-		 */
-		public function exists($index){ }
+		if(isset($this->_translate[$index]) === true) {
+			$translation = $this->_translate[$index];
+			if(is_array($placeholders) === true && empty($placeholders) === false) { 
+				foreach($placeholders as $key => $value) {
+					$translation = str_replace('%'.$key.'%', $value, $translation);
+				}
+			}
 
+			return $translation;
+		}
+
+		return $index;
+	}
+
+	/**
+	 * Check whether is defined a translation key in the internal array
+	 *
+	 * @param string $index
+	 * @return boolean
+	 * @throws Exception
+	 */
+	public function exists($index)
+	{
+		if(is_string($index) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
+
+		return isset($this->_translate[$index]);
 	}
 }

9a5647071b09f832a107e9dc2bb0456e9b0f098c
Wenzel Pünter <wenzel@phelix.me>
2014-04-27 13:38:25 +0200

    Flash Session implementation

--- Phalcon/Flash/Session.php
+++ Phalcon/Flash/Session.php
@@ -1,76 +1,215 @@
 <?php 
+/**
+ * Session Flash Messages
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Flash;
 
-namespace Phalcon\Flash {
+use \Phalcon\Flash,
+	\Phalcon\FlashInterface,
+	\Phalcon\DI\InjectionAwareInterface,
+	\Phalcon\Flash\Exception,
+	\Phalcon\DiInterface,
+	\Phalcon\Session\AdapterInterface as SessionAdapterInterface;
+
+/**
+ * Phalcon\Flash\Session
+ *
+ * Temporarily stores the messages in session, then messages can be printed in the next request
+ */
+class Session extends Flash implements FlashInterface, InjectionAwareInterface
+{
+	/**
+	 * Denpendency Injector
+	 * 
+	 * @var null|\Phalcon\DiInterface
+	 * @access protected
+	*/
+	protected $_dependencyInjector;
 
 	/**
-	 * Phalcon\Flash\Session
+	 * Sets the dependency injector
 	 *
-	 * Temporarily stores the messages in session, then messages can be printed in the next request
+	 * @param \Phalcon\DiInterface $dependencyInjector
+	 * @throws Exception
 	 */
-	
-	class Session extends \Phalcon\Flash implements \Phalcon\FlashInterface, \Phalcon\DI\InjectionAwareInterface {
+	public function setDI($dependencyInjector)
+	{
+		if(is_object($dependencyInjector) === false ||
+			$dependencyInjector instanceof DiInterface === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_dependencyInjector;
+		$this->_dependencyInjector = $dependencyInjector;
+	}
 
-		/**
-		 * Sets the dependency injector
-		 *
-		 * @param \Phalcon\DiInterface $dependencyInjector
-		 */
-		public function setDI($dependencyInjector){ }
+	/**
+	 * Returns the internal dependency injector
+	 *
+	 * @return \Phalcon\DiInterface|null
+	 */
+	public function getDI()
+	{
+		return $this->_dependencyInjector;
+	}
 
+	/**
+	 * Returns the messages stored in session
+	 *
+	 * @param boolean $remove
+	 * @return mixed
+	 * @throws Exception
+	 */
+	protected function _getSessionMessages($remove)
+	{
+		if(is_bool($remove) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Returns the internal dependency injector
-		 *
-		 * @return \Phalcon\DiInterface
-		 */
-		public function getDI(){ }
+		if(is_object($this->_dependencyInjector) === false) {
+			throw new Exception('A dependency injection container is required to access the \'session\' service');
+		}
 
+		$session = $this->_dependencyInjector->getShared('session');
+		if(is_object($session) === false ||
+			$session instanceof SessionAdapterInterface === false) {
+			throw new Exception('Session service is unavailable.');
+		}
 
-		/**
-		 * Returns the messages stored in session
-		 *
-		 * @param boolean $remove
-		 * @return array
-		 */
-		protected function _getSessionMessages(){ }
+		$messages = $session->get('_flashMessages');
 
+		if($remove === true) {
+			$session->remove('_flashMessages');
+		}
 
-		/**
-		 * Stores the messages in session
-		 *
-		 * @param array $messages
-		 */
-		protected function _setSessionMessages(){ }
+		return $messages;
+	}
 
+	/**
+	 * Stores the messages in session
+	 *
+	 * @param array $messages
+	 * @throws Exception
+	 * @return array
+	 */
+	protected function _setSessionMessages($messages)
+	{
+		if(is_array($messages) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Adds a message to the session flasher
-		 *
-		 * @param string $type
-		 * @param string $message
-		 */
-		public function message($type, $message){ }
+		if(is_object($this->_dependencyInjector) === false) {
+			throw new Exception('A dependency injection container is required to access the \'session\' service');
+		}
 
+		$session = $this->_dependencyInjector->getShared('session');
+		if(is_object($session) === false ||
+			$session instanceof SessionAdapterInterface === false) {
+			throw new Exception('Session service is unavailable.');
+		}
 
-		/**
-		 * Returns the messages in the session flasher
-		 *
-		 * @param string $type
-		 * @param boolean $remove
-		 * @return array
-		 */
-		public function getMessages($type=null, $remove=null){ }
+		$session->set('_flashMessages', $messages);
 
+		return $messages;
+	}
 
-		/**
-		 * Prints the messages in the session flasher
-		 *
-		 * @param string $type
-		 * @param boolean $remove
-		 */
-		public function output($remove=null){ }
+	/**
+	 * Adds a message to the session flasher
+	 *
+	 * @param string $type
+	 * @param string $message
+	 * @throws Exception
+	 */
+	public function message($type, $message)
+	{
+		if(is_string($type) === false ||
+			is_string($message) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		$messages = $this->_getSessionMessages(false);
+
+		if(is_array($messages) === false) {
+			$messages = array();
+		}
+
+		if(isset($messages[$type]) === false) {
+			$messages[$type] = array();
+		}
+
+		$messages[$type][] = $message;
+		$this->_setSessionMessages($messages);
+	}
+
+	/**
+	 * Returns the messages in the session flasher
+	 *
+	 * @param string|null $type
+	 * @param boolean|null $remove
+	 * @return array
+	 * @throws Exception
+	 */
+	public function getMessages($type = null, $remove = null)
+	{
+		if(is_null($remove) === true) {
+			$remove = true;
+		}
+
+		$do_remove = $remove;
+		if(is_string($type) === true) {
+			$do_remove = false;
+		} elseif(is_null($type) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
+
+		$messages = $this->_getSessionMessages($do_remove);
+
+		if(is_array($messages) === true) {
+			if(is_null($type) === false) {
+				if(isset($messages[$type]) === true) {
+					$return_messages = $messages[$type];
+
+					if($remove === true) {
+						unset($messages[$type]);
+						$this->_setSessionMessages($messages);
+					}
+
+					return $return_messages;
+				}
+
+				return array();
+			}
+
+			return $messages;
+		}
+
+		return array();
+	}
+
+	/**
+	 * Prints the messages in the session flasher
+	 *
+	 * @param boolean $remove
+	 * @throws Exception
+	 */
+	public function output($remove = null)
+	{
+		if(is_null($remove) === true) {
+			$remove = true;
+		} elseif(is_bool($remove) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
+
+		$messages = $this->_getSessionMessages($remove);
+		if(is_array($messages) === true) {
+			foreach($messages as $type => $message) {
+				$this->outputMessage($type, $message);
+			}
+		}
 	}
 }

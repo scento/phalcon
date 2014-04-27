607f6b7f85d636e58dd508a87ce667211dbea5e1
Wenzel Pünter <wenzel@phelix.me>
2014-04-27 13:20:31 +0200

    Direct Flash implementation

--- Phalcon/Flash/Direct.php
+++ Phalcon/Flash/Direct.php
@@ -1,23 +1,36 @@
-<?php 
+<?php
+/**
+ * Direct Flash Messages
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Flash;
 
-namespace Phalcon\Flash {
+use \Phalcon\Flash,
+	\Phalcon\FlashInterface;
 
+/**
+ * Phalcon\Flash\Direct
+ *
+ * This is a variant of the Phalcon\Flash that inmediately outputs any message passed to it
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/flash/direct.c
+ */
+class Direct extends Flash implements FlashInterface
+{
 	/**
-	 * Phalcon\Flash\Direct
+	 * Outputs a message
 	 *
-	 * This is a variant of the Phalcon\Flash that inmediately outputs any message passed to it
+	 * @param string $type
+	 * @param string $message
+	 * @return string
 	 */
-	
-	class Direct extends \Phalcon\Flash implements \Phalcon\FlashInterface {
-
-		/**
-		 * Outputs a message
-		 *
-		 * @param  string $type
-		 * @param  string $message
-		 * @return string
-		 */
-		public function message($type, $message){ }
-
+	public function message($type, $message)
+	{
+		return $this->outputMessage($type, $message);
 	}
 }

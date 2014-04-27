e26030d6c37bac89b9bc745c7f3467030ab677fc
Wenzel Pünter <wenzel@phelix.me>
2014-04-27 16:52:24 +0200

    Translate Exception restructure

--- Phalcon/Translate/Exception.php
+++ Phalcon/Translate/Exception.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
+/**
+ * Translate Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Translate;
 
-namespace Phalcon\Translate {
-
-	/**
-	 * Phalcon\Translate\Exception
-	 *
-	 * Class for exceptions thrown by Phalcon\Translate
-	 */
+/**
+ * Phalcon\Translate\Exception
+ *
+ * Class for exceptions thrown by Phalcon\Translate
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/translate/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

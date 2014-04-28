f32028e9f102f30a3737668bbfd814ce05a3428a
Wenzel Pünter <wenzel@phelix.me>
2014-04-28 22:05:55 +0200

    Validation Exception restructure

--- Phalcon/Validation/Exception.php
+++ Phalcon/Validation/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Validation Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Validation;
 
-namespace Phalcon\Validation {
+/**
+ * Phalcon\Validation\Exception
+ *
+ * Exceptions thrown in Phalcon\Validation\* classes will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/validation/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Validation\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Validation\* classes will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

7cd9c6e3270420a5df7016b8ae128d36a4abe93c
Wenzel Pünter <wenzel@phelix.me>
2014-04-28 21:54:17 +0200

    Events Exception restructure

--- Phalcon/Events/Exception.php
+++ Phalcon/Events/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Events Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Events;
 
-namespace Phalcon\Events {
+/**
+ * Phalcon\Events\Exception
+ *
+ * Exceptions thrown in Phalcon\Events will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/events/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Events\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Events will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

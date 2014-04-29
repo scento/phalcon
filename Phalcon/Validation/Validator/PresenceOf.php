adc16be8bb18540ae3d599dca2cd7d203c4ba172
Wenzel Pünter <wenzel@phelix.me>
2014-04-29 20:24:38 +0200

    PresenceOf Validator implementation

--- Phalcon/Validation/Validator/PresenceOf.php
+++ Phalcon/Validation/Validator/PresenceOf.php
@@ -1,31 +1,70 @@
-<?php 
+<?php
+/**
+ * PresenceOf Validator
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Validation\Validator;
 
-namespace Phalcon\Validation\Validator {
+use \Phalcon\Validation\Validator,
+	\Phalcon\Validation\ValidatorInterface,
+	\Phalcon\Validation\Message,
+	\Phalcon\Validation\Exception,
+	\Phalcon\Validation;
 
+/**
+ * Phalcon\Validation\Validator\PresenceOf
+ *
+ * Validates that a value is not null or empty string
+ *
+ *<code>
+ *use Phalcon\Validation\Validator\PresenceOf;
+ *
+ *$validator->add('name', new PresenceOf(array(
+ *   'message' => 'The name is required'
+ *)));
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/validation/validator/presenceof.c
+ */
+class PresenceOf extends Validator implements ValidatorInterface
+{
 	/**
-	 * Phalcon\Validation\Validator\PresenceOf
+	 * Executes the validation
 	 *
-	 * Validates that a value is not null or empty string
-	 *
-	 *<code>
-	 *use Phalcon\Validation\Validator\PresenceOf;
-	 *
-	 *$validator->add('name', new PresenceOf(array(
-	 *   'message' => 'The name is required'
-	 *)));
-	 *</code>
+	 * @param \Phalcon\Validation $validator
+	 * @param string $attribute
+	 * @return boolean
+	 * @throws Exception
 	 */
-	
-	class PresenceOf extends \Phalcon\Validation\Validator implements \Phalcon\Validation\ValidatorInterface {
+	public function validate($validator, $attribute)
+	{
+		if(is_object($validator) === false ||
+			$validator instanceof Validation === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Executes the validation
-		 *
-		 * @param \Phalcon\Validation $validator
-		 * @param string $attribute
-		 * @return boolean
-		 */
-		public function validate($validator, $attribute){ }
+		if(is_string($attribute) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		$value = $validator->getValue($attribute);
+		if(empty($value) === true) {
+			$message = $this->getOption('message');
+
+			if(empty($message) === true) {
+				$message = $attribute.' is required';
+			}
+
+			$validator->appendMessage(new Message($message, $attribute, 'PresenceOf'));
+
+			return false;
+		}
+
+		return true;
 	}
 }

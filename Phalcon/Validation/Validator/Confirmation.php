a27a110c19be3cbfbfab6468d36f6f660bd6ddda
Wenzel Pünter <wenzel@phelix.me>
2014-04-29 17:55:26 +0200

    Confirmation Validator implementation

--- Phalcon/Validation/Validator/Confirmation.php
+++ Phalcon/Validation/Validator/Confirmation.php
@@ -1,6 +1,19 @@
-<?php 
+<?php
+/**
+ * Confirmation Validator
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Validation\Validator;
 
-namespace Phalcon\Validation\Validator {
+use \Phalcon\Validation\Validator,
+	\Phalcon\Validation\ValidatorInterface,
+	\Phalcon\Validation\Exception,
+	\Phalcon\Validation\Message;
 
 	/**
 	 * Phalcon\Validation\Validator\Confirmation
@@ -15,18 +28,43 @@ namespace Phalcon\Validation\Validator {
 	 *   'with' => 'confirmPassword'
 	 *)));
 	 *</code>
+	 * 
+	 * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/validation/validator/confirmation.c
 	 */
 	
-	class Confirmation extends \Phalcon\Validation\Validator implements \Phalcon\Validation\ValidatorInterface {
-
+	class Confirmation extends Validator implements ValidatorInterface
+	{
 		/**
 		 * Executes the validation
 		 *
 		 * @param \Phalcon\Validation $validator
 		 * @param string $attribute
 		 * @return boolean
+		 * @throws Exception
 		 */
-		public function validate($validator, $attribute){ }
+		public function validate($validator, $attribute)
+		{
+			if(is_object($validator) === false ||
+				$validator instanceof Validation === false) {
+				throw new Exception('Invalid parameter type.');
+			}
 
+			if(is_string($attribute) === false) {
+				throw new Exception('Invalid parameter type.');
+			}
+
+			$with_attribute = $this->getOption('with');
+			$value = $validator->getValue($attribute);
+			if($value !== $with_attribute) {
+				$message = $this->getOption('message');
+				if(empty($message) === true) {
+					$message = 'Value of \''.$attribute.'\' and \''.$with_attribute.'\' don\'t match';
+				}
+
+				$validator->appendMessage(new Message($message, $attribute, 'Confirmation'));
+				return false;
+			}
+
+			return true;
+		}
 	}
-}

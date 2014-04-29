501675a565fde967dd054287ee26656bce4caa08
Wenzel Pünter <wenzel@phelix.me>
2014-04-29 17:57:05 +0200

    Changed code layout

--- Phalcon/Validation/Validator/Between.php
+++ Phalcon/Validation/Validator/Between.php
@@ -15,55 +15,55 @@ use \Phalcon\Validation\Validator,
 	\Phalcon\Validation\Exception,
 	\Phalcon\Validation\Message;
 
+/**
+ * Phalcon\Validation\Validator\Between
+ *
+ * Validates that a value is between a range of two values
+ *
+ *<code>
+ *use Phalcon\Validation\Validator\Between;
+ *
+ *$validator->add('name', new Between(array(
+ *   'minimum' => 0,
+ *   'maximum' => 100,
+ *   'message' => 'The price must be between 0 and 100'
+ *)));
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/validation/validator/between.c
+ */
+class Between extends Validator implements ValidatorInterface
+{
 	/**
-	 * Phalcon\Validation\Validator\Between
+	 * Executes the validation
 	 *
-	 * Validates that a value is between a range of two values
-	 *
-	 *<code>
-	 *use Phalcon\Validation\Validator\Between;
-	 *
-	 *$validator->add('name', new Between(array(
-	 *   'minimum' => 0,
-	 *   'maximum' => 100,
-	 *   'message' => 'The price must be between 0 and 100'
-	 *)));
-	 *</code>
-	 * 
-	 * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/validation/validator/between.c
+	 * @param \Phalcon\Validation $validator
+	 * @param string $attribute
+	 * @return boolean
+	 * @throws Exception
 	 */
-	class Between extends Validator implements ValidatorInterface
+	public function validate($validator, $attribute)
 	{
-		/**
-		 * Executes the validation
-		 *
-		 * @param \Phalcon\Validation $validator
-		 * @param string $attribute
-		 * @return boolean
-		 * @throws Exception
-		 */
-		public function validate($validator, $attribute)
-		{
-			if(is_object($validator) === false ||
-				$validator instanceof Validation === false) {
-				throw new Exception('Invalid parameter type.');
-			}
-
-			$value = $validator->getValue($attribute);
-			$minimum = $this->getOption('minimum');
-			$maximum = $this->getOption('maximum');
-
-			if($value <= $minimum || $value >= $maximum) {
-				$message_str = $this->getOption('message');
-				if(empty($message_str) === true) {
-					$message_str = $attribute.' is not between a valid range';
-				}
-
-				$validator->appendMessage(new Message($message_str, $attribute, 'Between'));
-
-				return false;
-			}
-
-			return true;
+		if(is_object($validator) === false ||
+			$validator instanceof Validation === false) {
+			throw new Exception('Invalid parameter type.');
 		}
+
+		$value = $validator->getValue($attribute);
+		$minimum = $this->getOption('minimum');
+		$maximum = $this->getOption('maximum');
+
+		if($value <= $minimum || $value >= $maximum) {
+			$message_str = $this->getOption('message');
+			if(empty($message_str) === true) {
+				$message_str = $attribute.' is not between a valid range';
+			}
+
+			$validator->appendMessage(new Message($message_str, $attribute, 'Between'));
+
+			return false;
+		}
+
+		return true;
 	}
+}
--- Phalcon/Validation/Validator/Confirmation.php
+++ Phalcon/Validation/Validator/Confirmation.php
@@ -15,56 +15,55 @@ use \Phalcon\Validation\Validator,
 	\Phalcon\Validation\Exception,
 	\Phalcon\Validation\Message;
 
+/**
+ * Phalcon\Validation\Validator\Confirmation
+ *
+ * Checks that two values have the same value
+ *
+ *<code>
+ *use Phalcon\Validation\Validator\Confirmation;
+ *
+ *$validator->add('password', new Confirmation(array(
+ *   'message' => 'Password doesn\'t match confirmation',
+ *   'with' => 'confirmPassword'
+ *)));
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/validation/validator/confirmation.c
+ */
+class Confirmation extends Validator implements ValidatorInterface
+{
 	/**
-	 * Phalcon\Validation\Validator\Confirmation
+	 * Executes the validation
 	 *
-	 * Checks that two values have the same value
-	 *
-	 *<code>
-	 *use Phalcon\Validation\Validator\Confirmation;
-	 *
-	 *$validator->add('password', new Confirmation(array(
-	 *   'message' => 'Password doesn\'t match confirmation',
-	 *   'with' => 'confirmPassword'
-	 *)));
-	 *</code>
-	 * 
-	 * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/validation/validator/confirmation.c
+	 * @param \Phalcon\Validation $validator
+	 * @param string $attribute
+	 * @return boolean
+	 * @throws Exception
 	 */
-	
-	class Confirmation extends Validator implements ValidatorInterface
+	public function validate($validator, $attribute)
 	{
-		/**
-		 * Executes the validation
-		 *
-		 * @param \Phalcon\Validation $validator
-		 * @param string $attribute
-		 * @return boolean
-		 * @throws Exception
-		 */
-		public function validate($validator, $attribute)
-		{
-			if(is_object($validator) === false ||
-				$validator instanceof Validation === false) {
-				throw new Exception('Invalid parameter type.');
-			}
-
-			if(is_string($attribute) === false) {
-				throw new Exception('Invalid parameter type.');
-			}
-
-			$with_attribute = $this->getOption('with');
-			$value = $validator->getValue($attribute);
-			if($value !== $with_attribute) {
-				$message = $this->getOption('message');
-				if(empty($message) === true) {
-					$message = 'Value of \''.$attribute.'\' and \''.$with_attribute.'\' don\'t match';
-				}
-
-				$validator->appendMessage(new Message($message, $attribute, 'Confirmation'));
-				return false;
-			}
-
-			return true;
+		if(is_object($validator) === false ||
+			$validator instanceof Validation === false) {
+			throw new Exception('Invalid parameter type.');
 		}
+
+		if(is_string($attribute) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
+
+		$with_attribute = $this->getOption('with');
+		$value = $validator->getValue($attribute);
+		if($value !== $with_attribute) {
+			$message = $this->getOption('message');
+			if(empty($message) === true) {
+				$message = 'Value of \''.$attribute.'\' and \''.$with_attribute.'\' don\'t match';
+			}
+
+			$validator->appendMessage(new Message($message, $attribute, 'Confirmation'));
+			return false;
+		}
+
+		return true;
 	}
+}

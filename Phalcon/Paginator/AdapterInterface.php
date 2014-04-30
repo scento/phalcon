bd4bcd0e98c44ef929bc84e66aa99f4bb5277007
Wenzel Pünter <wenzel@phelix.me>
2014-04-30 17:28:01 +0200

    Paginator Adapter Interface restructure

--- Phalcon/Paginator/AdapterInterface.php
+++ Phalcon/Paginator/AdapterInterface.php
@@ -1,35 +1,39 @@
-<?php 
+<?php
+/**
+ * Paginator Adapter Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Paginator;
 
-namespace Phalcon\Paginator {
+/**
+ * Phalcon\Paginator\AdapterInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/paginator/adapterinterface.c
+ */
+interface AdapterInterface
+{
+	/**
+	 * \Phalcon\Paginator\AdapterInterface constructor
+	 *
+	 * @param array $config
+	 */
+	public function __construct($config);
 
 	/**
-	 * Phalcon\Paginator\AdapterInterface initializer
+	 * Set the current page number
+	 *
+	 * @param int $page
 	 */
-	
-	interface AdapterInterface {
+	public function setCurrentPage($page);
 
-		/**
-		 * \Phalcon\Paginator\AdapterInterface constructor
-		 *
-		 * @param array $config
-		 */
-		public function __construct($config);
-
-
-		/**
-		 * Set the current page number
-		 *
-		 * @param int $page
-		 */
-		public function setCurrentPage($page);
-
-
-		/**
-		 * Returns a slice of the resultset to show in the pagination
-		 *
-		 * @return stdClass
-		 */
-		public function getPaginate();
-
-	}
+	/**
+	 * Returns a slice of the resultset to show in the pagination
+	 *
+	 * @return stdClass
+	 */
+	public function getPaginate();
 }

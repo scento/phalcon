f825e3128d98fd8880cad1800cd0b545aae5cdb7
Wenzel Pünter <wenzel@phelix.me>
2014-04-30 17:27:05 +0200

    Paginator Exception restructure

--- Phalcon/Paginator/Exception.php
+++ Phalcon/Paginator/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Paginator Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Paginator;
 
-namespace Phalcon\Paginator {
+/**
+ * Phalcon\Paginator\Exception
+ *
+ * Exceptions thrown in Phalcon\Paginator will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/paginator/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Paginator\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Paginator will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

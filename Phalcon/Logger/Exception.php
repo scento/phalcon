b331889a872ce793f31361177cdc17429f68fb6e
Wenzel Pünter <wenzel@phelix.me>
2014-05-01 18:05:57 +0200

    Logging Exception restructure

--- Phalcon/Logger/Exception.php
+++ Phalcon/Logger/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Logging Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Logger;
 
-namespace Phalcon\Logger {
+/**
+ * Phalcon\Logger\Exception
+ *
+ * Exceptions thrown in Phalcon\Logger will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/logger/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Logger\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Logger will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

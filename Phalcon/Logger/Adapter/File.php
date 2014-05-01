fc55d47b5eb5cb4be4aa0030c703a992411c14fb
Wenzel Pünter <wenzel@phelix.me>
2014-05-01 20:50:59 +0200

    Logger File Adapter implementation

--- Phalcon/Logger/Adapter/File.php
+++ Phalcon/Logger/Adapter/File.php
@@ -1,69 +1,168 @@
-<?php 
+<?php
+/**
+ * File Adapter
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Logger\Adapter;
 
-namespace Phalcon\Logger\Adapter {
+use \Phalcon\Logger\Adapter,
+	\Phalcon\Logger\AdapterInterface,
+	\Phalcon\Logger\Exception,
+	\Phalcon\Logger\Formatter\Line;
+
+/**
+ * Phalcon\Logger\Adapter\File
+ *
+ * Adapter to store logs in plain text files
+ *
+ *<code>
+ *	$logger = new \Phalcon\Logger\Adapter\File("app/logs/test.log");
+ *	$logger->log("This is a message");
+ *	$logger->log("This is an error", \Phalcon\Logger::ERROR);
+ *	$logger->error("This is another error");
+ *	$logger->close();
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/logger/adapter/file.c
+ */
+class File extends Adapter implements AdapterInterface
+{
+	/**
+	 * File Handler
+	 * 
+	 * @var null|resource
+	 * @access protected
+	*/
+	protected $_fileHandler;
 
 	/**
-	 * Phalcon\Logger\Adapter\File
+	 * Path
+	 * 
+	 * @var null|string
+	 * @access protected
+	*/
+	protected $_path;
+
+	/**
+	 * Options
+	 * 
+	 * @var null|array
+	 * @access protected
+	*/
+	protected $_options;
+
+	/**
+	 * \Phalcon\Logger\Adapter\File constructor
 	 *
-	 * Adapter to store logs in plain text files
-	 *
-	 *<code>
-	 *	$logger = new \Phalcon\Logger\Adapter\File("app/logs/test.log");
-	 *	$logger->log("This is a message");
-	 *	$logger->log("This is an error", \Phalcon\Logger::ERROR);
-	 *	$logger->error("This is another error");
-	 *	$logger->close();
-	 *</code>
+	 * @param string $name
+	 * @param array|null $options
+	 * @throws Exception
 	 */
-	
-	class File extends \Phalcon\Logger\Adapter implements \Phalcon\Logger\AdapterInterface {
+	public function __construct($name, $options = null)
+	{
+		if(is_string($name) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_fileHandler;
+		if(is_null($options) === true) {
+			$options = array();
+		} elseif(is_array($options) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_path;
+		if(isset($options['mode']) === true) {
+			if(strpos($options['mode'], 'r') === false) {
+				throw new Exception('Logger must be opened in append or write mode');
+			}
+		} else {
+			$mode = 'ab';
+		}
 
-		protected $_options;
+		//We use 'fopen' to respect the open-basedir directive
+		$handler = fopen($name, $mode);
+		if($handler === false) {
+			throw new Exception("Can't open lo gfile at '".$name."'");
+		}
 
-		/**
-		 * \Phalcon\Logger\Adapter\File constructor
-		 *
-		 * @param string $name
-		 * @param array $options
-		 */
-		public function __construct($name, $options=null){ }
+		$this->_path = $name;
+		$this->_options = $options;
+		$this->_fileHandler = $handler;
+	}
 
+	/**
+	 * Returns the internal formatter
+	 *
+	 * @return \Phalcon\Logger\Formatter\Line
+	 */
+	public function getFormatter()
+	{
+		if(is_object($this->_formatter) === false) {
+			$this->_formatter = new Line();
+		}
 
-		/**
-		 * Returns the internal formatter
-		 *
-		 * @return \Phalcon\Logger\Formatter\Line
-		 */
-		public function getFormatter(){ }
+		return $this->_formatter;
+	}
 
+	/**
+	 * Writes the log to the file itself
+	 *
+	 * @param string $message
+	 * @param int $type
+	 * @param int $time
+	 * @throws Exception
+	 */
+	public function logInternal($message, $type, $time)
+	{
+		if(is_string($message) === false ||
+			is_int($type) === false ||
+			is_int($time) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Writes the log to the file itself
-		 *
-		 * @param string $message
-		 * @param int $type
-		 * @param int $time
-		 */
-		public function logInternal($message, $type, $time){ }
+		if(isset($this->_fileHandler) === false) {
+			throw new Exception('Cannot send message to the log because it is invalid');
+		}
 
+		$formatter = $this->getFormatter();
+		fwrite($this->_fileHandler, $formatter->format($message, $type, $time));
+	}
 
-		/**
-		 * Closes the logger
-		 *
-		 * @return boolean
-		 */
-		public function close(){ }
+	/**
+	 * Closes the logger
+	 *
+	 * @return boolean
+	 */
+	public function close()
+	{
+		return fclose($this->_fileHandler);
+	}
 
+	/**
+	 * Opens the internal file handler after unserialization
+	 * 
+	 * @throws Exception
+	 */
+	public function __wakeup()
+	{
+		if(is_string($this->_path) === false) {
+			throw new Exception("Invalid data passed to Phalcon\\Logger\\Adapter\\File::__wakeup()");
+		}
 
-		/**
-		 * Opens the internal file handler after unserialization
-		 *
-		 */
-		public function __wakeup(){ }
+		if(isset($$this->_options['mode']) === true) {
+			$mode = $$this->_options['mode'];
+			if(is_string($mode) === false) {
+				throw new Exception("Invalid data passed to Phalcon\\Logger\\Adapter\\File::__wakeup()");
+			}
+		} else {
+			$mode = 'ab';
+		}
 
+		//Re-open the file handler if the logger was serialized
+		$this->_fileHandler = fopen($this->_path, $mode);
 	}
 }

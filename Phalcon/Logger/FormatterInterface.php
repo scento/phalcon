fbb2aec43a8740c7ce821db0f304777a0edd174c
Wenzel Pünter <wenzel@phelix.me>
2014-05-01 18:06:46 +0200

    Logger Formatter Interface restructure

--- Phalcon/Logger/FormatterInterface.php
+++ Phalcon/Logger/FormatterInterface.php
@@ -1,21 +1,27 @@
-<?php 
+<?php
+/**
+ * Formatter Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Logger;
 
-namespace Phalcon\Logger {
-
+/**
+ * Phalcon\Logger\FormatterInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/logger/formatterinterface.c
+ */
+interface FormatterInterface
+{
 	/**
-	 * Phalcon\Logger\FormatterInterface initializer
+	 * Applies a format to a message before sent it to the internal log
+	 *
+	 * @param string $message
+	 * @param int $type
+	 * @param int $timestamp
 	 */
-	
-	interface FormatterInterface {
-
-		/**
-		 * Applies a format to a message before sent it to the internal log
-		 *
-		 * @param string $message
-		 * @param int $type
-		 * @param int $timestamp
-		 */
-		public function format($message, $type, $timestamp);
-
-	}
+	public function format($message, $type, $timestamp);
 }

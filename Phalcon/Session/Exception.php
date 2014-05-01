52398853141c72ac6859fdea809daa01a604bba8
Wenzel Pünter <wenzel@phelix.me>
2014-05-01 17:23:58 +0200

    Session Exception restructure

--- Phalcon/Session/Exception.php
+++ Phalcon/Session/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Session Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Session;
 
-namespace Phalcon\Session {
+/**
+ * Phalcon\Session\Exception
+ *
+ * Exceptions thrown in Phalcon\Session will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/session/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Session\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Session will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

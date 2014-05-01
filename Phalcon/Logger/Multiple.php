91bc04c334ad88e2f364715fa91f230f0327d60c
Wenzel Pünter <wenzel@phelix.me>
2014-05-01 20:21:44 +0200

    Multiple Logger implementation

--- Phalcon/Logger/Multiple.php
+++ Phalcon/Logger/Multiple.php
@@ -1,115 +1,200 @@
-<?php 
+<?php
+/**
+ * Multiple
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Logger;
 
-namespace Phalcon\Logger {
+use \Phalcon\Logger\Exception,
+	\Phalcon\Logger\AdapterInterface,
+	\Phalcon\Logger\FormatterInterface,
+	\Phalcon\Logger;
+
+/**
+ * Phalcon\Logger\Multiple
+ *
+ * Handles multiples logger handlers
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/logger/multiple.c
+ */
+class Multiple
+{
+	/**
+	 * Loggers
+	 * 
+	 * @var null|array
+	 * @access protected
+	*/
+	protected $_loggers;
 
 	/**
-	 * Phalcon\Logger\Multiple
+	 * Formatter
+	 * 
+	 * @var null|\Phalcon\Logger\FormatterInterface
+	 * @access protected
+	*/
+	protected $_formatter;
+
+	/**
+	 * Pushes a logger to the logger tail
 	 *
-	 * Handles multiples logger handlers
+	 * @param \Phalcon\Logger\AdapterInterface $logger
+	 * @throws Exception
 	 */
-	
-	class Multiple {
+	public function push($logger)
+	{
+		if(is_object($logger) === false ||
+			$logger instanceof AdapterInterface === false) {
+			throw new Exception('The logger is invalid');
+		}
 
-		protected $_loggers;
+		if(is_array($this->_loggers) === false) {
+			$this->_loggers = array();
+		}
 
-		protected $_formatter;
+		$this->_loggers[] = $logger;
+	}
 
-		/**
-		 * Pushes a logger to the logger tail
-		 *
-		 * @param \Phalcon\Logger\AdapterInterface $logger
-		 */
-		public function push($logger){ }
+	/**
+	 * Returns the registered loggers
+	 *
+	 * @return \Phalcon\Logger\AdapterInterface[]|null
+	 */
+	public function getLoggers()
+	{
+		return $this->_loggers;
+	}
 
+	/**
+	 * Sets a global formatter
+	 *
+	 * @param \Phalcon\Logger\FormatterInterface $formatter
+	 * @throws Exception
+	 */
+	public function setFormatter($formatter)
+	{
+		if(is_object($formatter) === false ||
+			$formatter instanceof FormatterInterface === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Returns the registered loggers
-		 *
-		 * @return \Phalcon\Logger\AdapterInterface[]
-		 */
-		public function getLoggers(){ }
+		if(is_array($this->_loggers) === true) {
+			foreach($this->_loggers as $logger) {
+				$logger->setFormatter($formatter);
+			}
+		}
 
+		$this->_formatter = $formatter;
+	}
 
-		/**
-		 * Sets a global formatter
-		 *
-		 * @param \Phalcon\Logger\FormatterInterface $formatter
-		 */
-		public function setFormatter($formatter){ }
+	/**
+	 * Returns a formatter
+	 *
+	 * @return \Phalcon\Logger\FormatterInterface|null
+	 */
+	public function getFormatter()
+	{
+		return $this->_formatter;
+	}
 
+	/**
+	 * Sends a message to each registered logger
+	 *
+	 * @param string $message
+	 * @param int $type
+	 * @throws Exception
+	 */
+	public function log($message, $type = null)
+	{
+		if(is_string($message) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Returns a formatter
-		 *
-		 * @return \Phalcon\Logger\FormatterInterface
-		 */
-		public function getFormatter(){ }
+		if(is_null($type) === true) {
+			$type = 7;
+		} elseif(is_int($type) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		if(is_array($this->_loggers) === true) {
+			foreach($this->_loggers as $logger) {
+				$logger->log($message, $type);
+			}
+		}
+	}
 
-		/**
-		 * Sends a message to each registered logger
-		 *
-		 * @param string $message
-		 * @param int $type
-		 */
-		public function log($message, $type=null){ }
+	/**
+	 * Sends/Writes an emergence message to the log
+	 *
+	 * @param string $message
+	 */
+	public function emergence($message)
+	{
+		$this->log($message, Logger::EMERGENCE);
+	}
 
+	/**
+	 * Sends/Writes a debug message to the log
+	 *
+	 * @param string $message
+	 */
+	public function debug($message)
+	{
+		$this->log($message, Logger::DEBUG);
+	}
 
-		/**
-		 * Sends/Writes an emergence message to the log
-		 *
-		 * @param string $message
-		 */
-		public function emergence($message){ }
+	/**
+	 * Sends/Writes an error message to the log
+	 *
+	 * @param string $message
+	 */
+	public function error($message)
+	{
+		$this->log($message, Logger::ERROR);
+	}
 
+	/**
+	 * Sends/Writes an info message to the log
+	 *
+	 * @param string $message
+	 */
+	public function info($message)
+	{
+		$this->log($message, Logger::INFO);
+	}
 
-		/**
-		 * Sends/Writes a debug message to the log
-		 *
-		 * @param string $message
-		 * @param ing $type
-		 */
-		public function debug($message){ }
+	/**
+	 * Sends/Writes a notice message to the log
+	 *
+	 * @param string $message
+	 */
+	public function notice($message)
+	{
+		$this->log($message, Logger::NOTICE);
+	}
 
+	/**
+	 * Sends/Writes a warning message to the log
+	 *
+	 * @param string $message
+	 */
+	public function warning($message)
+	{
+		$this->log($message, Logger::WARNING);
+	}
 
-		/**
-		 * Sends/Writes an error message to the log
-		 *
-		 * @param string $message
-		 */
-		public function error($message){ }
-
-
-		/**
-		 * Sends/Writes an info message to the log
-		 *
-		 * @param string $message
-		 */
-		public function info($message){ }
-
-
-		/**
-		 * Sends/Writes a notice message to the log
-		 *
-		 * @param string $message
-		 */
-		public function notice($message){ }
-
-
-		/**
-		 * Sends/Writes a warning message to the log
-		 *
-		 * @param string $message
-		 */
-		public function warning($message){ }
-
-
-		/**
-		 * Sends/Writes an alert message to the log
-		 *
-		 * @param string $message
-		 */
-		public function alert($message){ }
-
+	/**
+	 * Sends/Writes an alert message to the log
+	 *
+	 * @param string $message
+	 */
+	public function alert($message)
+	{
+		$this->log($message, Logger::ALERT);
 	}
 }

64f136c377689cdf674dc8867f35858d344a48ec
Wenzel Pünter <wenzel@phelix.me>
2014-05-02 15:53:02 +0200

    Stream logging adapter implementation

--- Phalcon/Logger/Adapter/Stream.php
+++ Phalcon/Logger/Adapter/Stream.php
@@ -1,57 +1,117 @@
-<?php 
+<?php
+/**
+ * Stream Adapter
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Logger\Adapter;
 
-namespace Phalcon\Logger\Adapter {
+use \Phalcon\Logger\Adapter,
+	\Phalcon\Logger\AdapterInterface,
+	\Phalcon\Logger\Exception,
+	\Phalcon\Logger\Formatter\Line;
+
+/**
+ * Phalcon\Logger\Adapter\Stream
+ *
+ * Sends logs to a valid PHP stream
+ *
+ *<code>
+ *	$logger = new \Phalcon\Logger\Adapter\Stream("php://stderr");
+ *	$logger->log("This is a message");
+ *	$logger->log("This is an error", \Phalcon\Logger::ERROR);
+ *	$logger->error("This is another error");
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/logger/adapter/stream.c
+ */
+class Stream extends Adapter implements AdapterInterface
+{
+	/**
+	 * Stream
+	 * 
+	 * @var null|resource
+	 * @access protected
+	*/
+	protected $_stream;
 
 	/**
-	 * Phalcon\Logger\Adapter\Stream
+	 * \Phalcon\Logger\Adapter\Stream constructor
 	 *
-	 * Sends logs to a valid PHP stream
-	 *
-	 *<code>
-	 *	$logger = new \Phalcon\Logger\Adapter\Stream("php://stderr");
-	 *	$logger->log("This is a message");
-	 *	$logger->log("This is an error", \Phalcon\Logger::ERROR);
-	 *	$logger->error("This is another error");
-	 *</code>
+	 * @param string $name
+	 * @param array|null $options
+	 * @throws Exception
 	 */
-	
-	class Stream extends \Phalcon\Logger\Adapter implements \Phalcon\Logger\AdapterInterface {
+	public function __construct($name, $options = null)
+	{
+		if(is_string($name) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_stream;
+		if(is_null($options) === true) {
+			$mode = 'ab';
+		} elseif(is_array($options) === true) {
+			$mode = $options['mode'];
+			if(strpos($mode, 'r') === true) {
+				throw new Exception('Stream must be opened in append or write mode');
+			}
+		} else {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * \Phalcon\Logger\Adapter\Stream constructor
-		 *
-		 * @param string $name
-		 * @param array $options
-		 */
-		public function __construct($name, $options=null){ }
+		//We use 'fopen' to respect the open-basedir directive
+		$stream = fopen($name, $mode);
+		if(is_resource($stream) === false) {
+			throw new Exception("Can't open stream '".$name."'");
+		}
 
+		$this->_stream = $stream;
+	}
 
-		/**
-		 * Returns the internal formatter
-		 *
-		 * @return \Phalcon\Logger\Formatter\Line
-		 */
-		public function getFormatter(){ }
+	/**
+	 * Returns the internal formatter
+	 *
+	 * @return \Phalcon\Logger\Formatter\Line
+	 */
+	public function getFormatter()
+	{
+		if(is_object($this->_formatter) === false) {
+			$this->_formatter = new Line();
 
+		}
 
-		/**
-		 * Writes the log to the stream itself
-		 *
-		 * @param string $message
-		 * @param int $type
-		 * @param int $time
-		 */
-		public function logInternal($message, $type, $time){ }
+		return $this->_formatter;
+	}
 
+	/**
+	 * Writes the log to the stream itself
+	 *
+	 * @param string $message
+	 * @param int $type
+	 * @param int $time
+	 * @throws Exception
+	 */
+	public function logInternal($message, $type, $time)
+	{
+		if(is_resource($this->_stream) === false) {
+			throw new Exception('Cannot send message to the log because it is invalid');
+		}
 
-		/**
-		 * Closes the logger
-		 *
-		 * @return boolean
-		 */
-		public function close(){ }
+		//@note no return value handeling
+		fwrite($this->_stream, $this->getFormatter()->format($message, $type, $time));
+	}
 
+	/**
+	 * Closes the logger
+	 *
+	 * @return boolean
+	 */
+	public function close()
+	{
+		return fclose($this->_stream);
 	}
 }

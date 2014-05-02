36c63d8579c2c3f52d261ef3ba47cd511f2731e6
Wenzel Pünter <wenzel@phelix.me>
2014-05-02 16:30:55 +0200

    Syslog logging formatter implementation

--- Phalcon/Logger/Formatter/Syslog.php
+++ Phalcon/Logger/Formatter/Syslog.php
@@ -1,24 +1,45 @@
-<?php 
+<?php
+/**
+ * Syslog Formatter
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Logger\Formatter;
 
-namespace Phalcon\Logger\Formatter {
+use \Phalcon\Logger\Formatter,
+	\Phalcon\Logger\FormatterInterface,
+	\Phalcon\Logger\Exception;
 
+/**
+ * Phalcon\Logger\Formatter\Syslog
+ *
+ * Prepares a message to be used in a Syslog backend
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/logger/formatter/syslog.c
+ */
+class Syslog extends Formatter implements FormatterInterface
+{
 	/**
-	 * Phalcon\Logger\Formatter\Syslog
+	 * Applies a format to a message before sent it to the internal log
 	 *
-	 * Prepares a message to be used in a Syslog backend
+	 * @param string $message
+	 * @param int $type
+	 * @param int $timestamp
+	 * @return array
+	 * @throws Exception
 	 */
-	
-	class Syslog extends \Phalcon\Logger\Formatter implements \Phalcon\Logger\FormatterInterface {
+	public function format($message, $type, $timestamp)
+	{
+		if(is_string($message) === false ||
+			is_int($type) === false ||
+			is_int($timestamp) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Applies a format to a message before sent it to the internal log
-		 *
-		 * @param string $message
-		 * @param int $type
-		 * @param int $timestamp
-		 * @return array
-		 */
-		public function format($message, $type, $timestamp){ }
-
+		return array($type, $message);
 	}
 }

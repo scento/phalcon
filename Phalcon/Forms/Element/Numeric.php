83940e657b79b2c2e8f36a8e274ec661bb51c071
Wenzel Pünter <wenzel@phelix.me>
2014-05-05 16:26:27 +0200

    Numeric element class implementation

--- Phalcon/Forms/Element/Numeric.php
+++ Phalcon/Forms/Element/Numeric.php
@@ -1,22 +1,43 @@
-<?php 
+<?php
+/**
+ * Numeric
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Forms\Element;
 
-namespace Phalcon\Forms\Element {
+use \Phalcon\Tag,
+	\Phalcon\Forms\Element,
+	\Phalcon\Forms\ElementInterface,
+	\Phalcon\Forms\Exception;
 
+/**
+ * Phalcon\Forms\Element\Numeric
+ *
+ * Component INPUT[type=number] for forms
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/forms/element/numeric.c
+ */
+class Numeric extends Element implements ElementInterface
+{
 	/**
-	 * Phalcon\Forms\Element\Numeric
+	 * Renders the element widget returning html
 	 *
-	 * Component INPUT[type=number] for forms
+	 * @param array|null $attributes
+	 * @return string
+	 * @throws Exception
 	 */
-	
-	class Numeric extends \Phalcon\Forms\Element implements \Phalcon\Forms\ElementInterface {
+	public function render($attributes = null)
+	{
+		if(is_array($attributes) === false &&
+			is_null($attributes) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Renders the element widget returning html
-		 *
-		 * @param array $attributes
-		 * @return string
-		 */
-		public function render($attributes=null){ }
-
+		return Tag::numericField($this->prepareAttributes($attributes));
 	}
 }

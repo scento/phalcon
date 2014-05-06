9f1396084c8b28ad90a99d312199d5b37a8b2b4d
Wenzel Pünter <wenzel@phelix.me>
2014-05-06 20:33:33 +0200

    MVC dispatcher interface restructure

--- Phalcon/Mvc/DispatcherInterface.php
+++ Phalcon/Mvc/DispatcherInterface.php
@@ -1,60 +1,63 @@
-<?php 
+<?php
+/**
+ * Dispatcher Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc;
 
-namespace Phalcon\Mvc {
+use \Phalcon\DispatcherInterface;
+
+/**
+ * Phalcon\Mvc\DispatcherInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/dispatcherinterface.c
+ */
+interface DispatcherInterface extends \Phalcon\DispatcherInterface
+{
+	/**
+	 * Sets the default controller suffix
+	 *
+	 * @param string $controllerSuffix
+	 */
+	public function setControllerSuffix($controllerSuffix);
 
 	/**
-	 * Phalcon\Mvc\DispatcherInterface initializer
+	 * Sets the default controller name
+	 *
+	 * @param string $controllerName
 	 */
-	
-	interface DispatcherInterface extends \Phalcon\DispatcherInterface {
+	public function setDefaultController($controllerName);
 
-		/**
-		 * Sets the default controller suffix
-		 *
-		 * @param string $controllerSuffix
-		 */
-		public function setControllerSuffix($controllerSuffix);
+	/**
+	 * Sets the controller name to be dispatched
+	 *
+	 * @param string $controllerName
+	 * @param bool|null $isExact If true, the name should not be mangled in any way
+	 */
+	public function setControllerName($controllerName, $isExact = null);
 
+	/**
+	 * Gets last dispatched controller name
+	 *
+	 * @return string
+	 */
+	public function getControllerName();
 
-		/**
-		 * Sets the default controller name
-		 *
-		 * @param string $controllerName
-		 */
-		public function setDefaultController($controllerName);
+	/**
+	 * Returns the lastest dispatched controller
+	 *
+	 * @return \Phalcon\Mvc\ControllerInterface
+	 */
+	public function getLastController();
 
-
-		/**
-		 * Sets the controller name to be dispatched
-		 *
-		 * @param string $controllerName
-		 * @param bool $isExact If true, the name should not be mangled in any way
-		 */
-		public function setControllerName($controllerName, $isExact=null);
-
-
-		/**
-		 * Gets last dispatched controller name
-		 *
-		 * @return string
-		 */
-		public function getControllerName();
-
-
-		/**
-		 * Returns the lastest dispatched controller
-		 *
-		 * @return \Phalcon\Mvc\ControllerInterface
-		 */
-		public function getLastController();
-
-
-		/**
-		 * Returns the active controller in the dispatcher
-		 *
-		 * @return \Phalcon\Mvc\ControllerInterface
-		 */
-		public function getActiveController();
-
-	}
+	/**
+	 * Returns the active controller in the dispatcher
+	 *
+	 * @return \Phalcon\Mvc\ControllerInterface
+	 */
+	public function getActiveController();
 }

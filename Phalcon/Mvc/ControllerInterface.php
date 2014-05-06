efa0c8a5d8c566b03e4715d2c855c942122b231c
Wenzel Pünter <wenzel@phelix.me>
2014-05-06 20:34:47 +0200

    MVC controller interface restructure

--- Phalcon/Mvc/ControllerInterface.php
+++ Phalcon/Mvc/ControllerInterface.php
@@ -1,11 +1,20 @@
-<?php 
+<?php
+/**
+ * Controller Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc;
 
-namespace Phalcon\Mvc {
+/**
+ * Phalcon\Mvc\ControllerInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/controllerinterface.c
+ */
+interface ControllerInterface
+{
 
-	/**
-	 * Phalcon\Mvc\ControllerInterface initializer
-	 */
-	
-	interface ControllerInterface {
-	}
 }

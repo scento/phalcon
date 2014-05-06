40d36dfe32f785bff47faf6dae7b007cbec67eb5
Wenzel Pünter <wenzel@phelix.me>
2014-05-06 20:50:14 +0200

    MVC application exception restructure

--- Phalcon/Mvc/Application/Exception.php
+++ Phalcon/Mvc/Application/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Application Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Application;
 
-namespace Phalcon\Mvc\Application {
+/**
+ * Phalcon\Mvc\Application\Exception
+ *
+ * Exceptions thrown in Phalcon\Mvc\Application class will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/application/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Mvc\Application\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Mvc\Application class will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

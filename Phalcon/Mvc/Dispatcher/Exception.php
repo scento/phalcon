04457e15f5abdbbf367949a952b198d2ad51aebe
Wenzel Pünter <wenzel@phelix.me>
2014-05-06 20:51:05 +0200

    MVC dispatcher exception restructure

--- Phalcon/Mvc/Dispatcher/Exception.php
+++ Phalcon/Mvc/Dispatcher/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Dispatcher Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Dispatcher;
 
-namespace Phalcon\Mvc\Dispatcher {
-
-	/**
-	 * Phalcon\Mvc\Dispatcher\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Mvc\Dispatcher will use this class
-	 *
-	 */
+/**
+ * Phalcon\Mvc\Dispatcher\Exception
+ *
+ * Exceptions thrown in Phalcon\Mvc\Dispatcher will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/dispatcher/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

e3e602d872f7c2c4735be08ee4e677c5cefc4dbf
Wenzel Pünter <wenzel@phelix.me>
2014-05-18 13:20:45 +0200

    MVC user plugin restructure

--- Phalcon/Mvc/User/Plugin.php
+++ Phalcon/Mvc/User/Plugin.php
@@ -1,14 +1,27 @@
-<?php 
+<?php
+/**
+ * User Plugin
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\User;
 
-namespace Phalcon\Mvc\User {
+use \Phalcon\DI\Injectable,
+	\Phalcon\Events\EventsAwareInterface,
+	\Phalcon\DI\InjectionAwareInterface;
 
-	/**
-	 * Phalcon\Mvc\User\Plugin
-	 *
-	 * This class can be used to provide user plugins an easy access to services
-	 * in the application
-	 */
-	
-	class Plugin extends \Phalcon\DI\Injectable implements \Phalcon\Events\EventsAwareInterface, \Phalcon\DI\InjectionAwareInterface {
-	}
+/**
+ * Phalcon\Mvc\User\Plugin
+ *
+ * This class can be used to provide user plugins an easy access to services
+ * in the application
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/user/plugin.c
+ */
+class Plugin extends Injectable implements EventsAwareInterface, InjectionAwareInterface
+{
+
 }

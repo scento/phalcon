f1297430f760f88b0869d9f66069c9fbeffa1731
Wenzel Pünter <wenzel@phelix.me>
2014-05-18 13:19:47 +0200

    MVC user module restructure

--- Phalcon/Mvc/User/Module.php
+++ Phalcon/Mvc/User/Module.php
@@ -1,14 +1,27 @@
-<?php 
+<?php
+/**
+ * User Module
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\User;
 
-namespace Phalcon\Mvc\User {
+use \Phalcon\DI\Injectable,
+	\Phalcon\Events\EventsAwareInterface,
+	\Phalcon\DI\InjectionAwareInterface;
 
-	/**
-	 * Phalcon\Mvc\User\Module
-	 *
-	 * This class can be used to provide user modules easy access to services
-	 * in the application
-	 */
+/**
+ * Phalcon\Mvc\User\Module
+ *
+ * This class can be used to provide user modules easy access to services
+ * in the application
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/user/module.c
+ */
+class Module extends Injectable implements EventsAwareInterface, InjectionAwareInterface
+{
 	
-	class Module extends \Phalcon\DI\Injectable implements \Phalcon\Events\EventsAwareInterface, \Phalcon\DI\InjectionAwareInterface {
-	}
 }

dc9d273efa0ad556fe9903dc872be4cfd397b88f
Wenzel Pünter <wenzel@phelix.me>
2014-05-18 13:18:35 +0200

    MVC user component restructure

--- Phalcon/Mvc/User/Component.php
+++ Phalcon/Mvc/User/Component.php
@@ -1,14 +1,27 @@
-<?php 
+<?php
+/**
+ * User Component
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\User;
 
-namespace Phalcon\Mvc\User {
+use Phalcon\DI\Injectable,
+	\Phalcon\Events\EventsAwareInterface,
+	\Phalcon\DI\InjectionAwareInterface;
 
-	/**
-	 * Phalcon\Mvc\User\Component
-	 *
-	 * This class can be used to provide user components easy access to services
-	 * in the application
-	 */
-	
-	class Component extends \Phalcon\DI\Injectable implements \Phalcon\Events\EventsAwareInterface, \Phalcon\DI\InjectionAwareInterface {
-	}
+/**
+ * Phalcon\Mvc\User\Component
+ *
+ * This class can be used to provide user components easy access to services
+ * in the application
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/user/component.c
+ */
+class Component extends Injectable implements EventsAwareInterface, InjectionAwareInterface
+{
+
 }

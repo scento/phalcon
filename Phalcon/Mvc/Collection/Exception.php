b9616c6e66ff75096b1874672d28a33d6fff874b
Wenzel Pünter <wenzel@phelix.me>
2014-05-18 18:21:06 +0200

    MVC collection exception restructure

--- Phalcon/Mvc/Collection/Exception.php
+++ Phalcon/Mvc/Collection/Exception.php
@@ -1,14 +1,23 @@
-<?php 
+<?php
+/**
+ * Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Collection;
 
-namespace Phalcon\Mvc\Collection {
+/**
+ * Phalcon\Mvc\Collection\Exception
+ *
+ * Exceptions thrown in Phalcon\Mvc\Collection\* classes will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/collection/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Mvc\Collection\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Mvc\Collection\* classes will use this class
-	 *
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

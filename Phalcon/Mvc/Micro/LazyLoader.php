68a8f971e1c9e4bb5057042f9342dc5bb8cd470e
Wenzel Pünter <wenzel@phelix.me>
2014-05-21 15:09:01 +0200

    MVC micro lazy loader implementation

--- Phalcon/Mvc/Micro/LazyLoader.php
+++ Phalcon/Mvc/Micro/LazyLoader.php
@@ -1,35 +1,77 @@
-<?php 
+<?php
+/**
+ * Lazy Loader
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Micro;
 
-namespace Phalcon\Mvc\Micro {
+use \Phalcon\Mvc\Micro\Exception;
+
+/**
+ * Phalcon\Mvc\Micro\LazyLoader
+ *
+ * Lazy-Load of handlers for Mvc\Micro using auto-loading
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/micro/lazyloader.c
+ */
+class LazyLoader
+{
+	/**
+	 * Handler
+	 * 
+	 * @var null|object
+	 * @access protected
+	*/
+	protected $_handler;
 
 	/**
-	 * Phalcon\Mvc\Micro\LazyLoader
+	 * Definition
+	 * 
+	 * @var null|string
+	 * @access protected
+	*/
+	protected $_definition;
+
+	/**
+	 * \Phalcon\Mvc\Micro\LazyLoader constructor
 	 *
-	 * Lazy-Load of handlers for Mvc\Micro using auto-loading
+	 * @param string $definition
+	 * @throws Exception
 	 */
-	
-	class LazyLoader {
+	public function __construct($definition)
+	{
+		if(is_string($definition) === false) {
+			throw new Exception('Only strings can be lazy loaded');
+		}
 
-		protected $_handler;
+		$this->_definition = $definition;
+	}
 
-		protected $_definition;
+	/**
+	 * Initializes the internal handler, calling functions on it
+	 *
+	 * @param string $method
+	 * @param array $arguments
+	 * @return mixed
+	 * @throws Exception
+	 */
+	public function __call($method, $arguments)
+	{
+		if(is_string($method) === false ||
+			is_array($arguments) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * \Phalcon\Mvc\Micro\LazyLoader constructor
-		 *
-		 * @param string $definition
-		 */
-		public function __construct($definition){ }
+		if(is_object($this->_handler) === false) {
+			$this->_handler = new $_definition();
+		}
 
-
-		/**
-		 * Initializes the internal handler, calling functions on it
-		 *
-		 * @param string $method
-		 * @param array $arguments
-		 * @return mixed
-		 */
-		public function __call($method, $arguments){ }
-
+		//Call the handler
+		return call_user_func_array(array($this->_handler, $method), $arguments);
 	}
 }

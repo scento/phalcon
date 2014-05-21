3135afe7b51fc06e1eab49af168a806bc9c1c928
Wenzel Pünter <wenzel@phelix.me>
2014-05-21 14:58:17 +0200

    MVC micro collection implementation

--- Phalcon/Mvc/Micro/Collection.php
+++ Phalcon/Mvc/Micro/Collection.php
@@ -1,176 +1,340 @@
-<?php 
+<?php
+/**
+ * Micro Collection
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Micro;
 
-namespace Phalcon\Mvc\Micro {
+use \Phalcon\Mvc\Micro\CollectionInterface,
+	\Phalcon\Mvc\Micro\Exception;
+
+/**
+ * Phalcon\Mvc\Micro\Collection
+ *
+ * Groups Micro-Mvc handlers as controllers
+ *
+ *<code>
+ *
+ * $app = new Phalcon\Mvc\Micro();
+ *
+ * $collection = new Phalcon\Mvc\Micro\Collection();
+ *
+ * $collection->setHandler(new PostsController());
+ *
+ * $collection->get('/posts/edit/{id}', 'edit');
+ *
+ * $app->mount($collection);
+ *
+ *</code>
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/micro/collection.c
+ */
+class Collection implements \Phalcon\Mvc\Micro\CollectionInterface
+{
+	/**
+	 * Prefix
+	 * 
+	 * @var null|string
+	 * @access protected
+	*/
+	protected $_prefix;
 
 	/**
-	 * Phalcon\Mvc\Micro\Collection
-	 *
-	 * Groups Micro-Mvc handlers as controllers
-	 *
-	 *<code>
-	 *
-	 * $app = new Phalcon\Mvc\Micro();
-	 *
-	 * $collection = new Phalcon\Mvc\Micro\Collection();
-	 *
-	 * $collection->setHandler(new PostsController());
-	 *
-	 * $collection->get('/posts/edit/{id}', 'edit');
-	 *
-	 * $app->mount($collection);
-	 *
-	 *</code>
-	 *
-	 */
+	 * Lazy
+	 * 
+	 * @var null|boolean
+	 * @access protected
+	*/
+	protected $_lazy;
+
+	/**
+	 * Handler
+	 * 
+	 * @var null|mixed
+	 * @access protected
+	*/
+	protected $_handler;
+
+	/**
+	 * Handlers
+	 * 
+	 * @var null|array
+	 * @access protected
+	*/
+	protected $_handlers;
 	
-	class Collection implements \Phalcon\Mvc\Micro\CollectionInterface {
+	/**
+	 * Add a hander to the group
+	 * 
+	 * @param string|array $method
+	 * @param string $routePattern
+	 * @param mixed $handler
+	*/
+	private function addToMap($method, $routePattern, $handler)
+	{
+		$this->_handlers[] = array($method, $routePattern, $handler);
+	}
 
-		protected $_prefix;
+	/**
+	 * Sets a prefix for all routes added to the collection
+	 *
+	 * @param string $prefix
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function setPrefix($prefix)
+	{
+		if(is_string($prefix) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_lazy;
+		$this->_prefix = $prefix;
 
-		protected $_handler;
+		return $this;
+	}
 
-		protected $_handlers;
+	/**
+	 * Returns the collection prefix if any
+	 *
+	 * @return string|null
+	 */
+	public function getPrefix()
+	{
+		return $this->_prefix;
+	}
 
-		/**
-		 * Sets a prefix for all routes added to the collection
-		 *
-		 * @param string $prefix
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function setPrefix($prefix){ }
+	/**
+	 * Returns the registered handlers
+	 *
+	 * @return array|null
+	 */
+	public function getHandlers()
+	{
+		return $this->_handlers;
+	}
 
+	/**
+	 * Sets the main handler
+	 *
+	 * @param mixed $handler
+	 * @param boolean|null $lazy
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function setHandler($handler, $lazy = null)
+	{
+		if(is_null($lazy) === true) {
+			$lazy = false;
+		} elseif(is_bool($lazy) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Returns the collection prefix if any
-		 *
-		 * @return string
-		 */
-		public function getPrefix(){ }
+		$this->_handlers = $handler;
+		$this->_lazy = $lazy;
 
+		return $this;
+	}
 
-		/**
-		 * Returns the registered handlers
-		 *
-		 * @return array
-		 */
-		public function getHandlers(){ }
+	/**
+	 * Sets if the main handler must be lazy loaded
+	 *
+	 * @param boolean $lazy
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function setLazy($lazy)
+	{
+		if(is_bool($lazy) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		$this->_lazy = $lazy;
 
-		/**
-		 * Sets the main handler
-		 *
-		 * @param mixed $handler
-		 * @param boolean $lazy
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function setHandler($handler, $lazy=null){ }
+		return $this;
+	}
 
+	/**
+	 * Returns if the main handler must be lazy loaded
+	 *
+	 * @return boolean|null
+	 */
+	public function isLazy()
+	{
+		return $this->_lazy;
+	}
 
-		/**
-		 * Sets if the main handler must be lazy loaded
-		 *
-		 * @param boolean $lazy
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function setLazy($lazy){ }
+	/**
+	 * Returns the main handler
+	 *
+	 * @return mixed
+	 */
+	public function getHandler()
+	{
+		return $this->_handler;
+	}
 
+	/**
+	 * Maps a route to a handler
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function map($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Returns if the main handler must be lazy loaded
-		 *
-		 * @return boolean
-		 */
-		public function isLazy(){ }
+		$this->addToMap(null, $routePattern, $handler);
 
+		return $this;
+	}
 
-		/**
-		 * Returns the main handler
-		 *
-		 * @return mixed
-		 */
-		public function getHandler(){ }
+	/**
+	 * Maps a route to a handler that only matches if the HTTP method is GET
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function get($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		$this->addToMap('GET', $routePattern, $handler);
 
-		/**
-		 * Maps a route to a handler
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function map($routePattern, $handler){ }
+		return $this;
+	}
 
+	/**
+	 * Maps a route to a handler that only matches if the HTTP method is POST
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function post($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Maps a route to a handler that only matches if the HTTP method is GET
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function get($routePattern, $handler){ }
+		$this->addToMap('POST', $routePattern, $handler);
 
+		return $this;
+	}
 
-		/**
-		 * Maps a route to a handler that only matches if the HTTP method is POST
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function post($routePattern, $handler){ }
+	/**
+	 * Maps a route to a handler that only matches if the HTTP method is PUT
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function put($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		$this->addToMap('PUT', $routePattern, $handler);
 
-		/**
-		 * Maps a route to a handler that only matches if the HTTP method is PUT
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function put($routePattern, $handler){ }
+		return $this;
+	}
 
+	/**
+	 * Maps a route to a handler that only matches if the HTTP method is PATCH
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function patch($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Maps a route to a handler that only matches if the HTTP method is PATCH
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function patch($routePattern, $handler){ }
+		$this->addToMap('PATCH', $routePattern, $handler);
 
+		return $this;
+	}
 
-		/**
-		 * Maps a route to a handler that only matches if the HTTP method is HEAD
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function head($routePattern, $handler){ }
+	/**
+	 * Maps a route to a handler that only matches if the HTTP method is HEAD
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function head($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		$this->addToMap('HEAD', $routePattern, $handler);
 
-		/**
-		 * Maps a route to a handler that only matches if the HTTP method is DELETE
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function delete($routePattern, $handler){ }
+		return $this;
+	}
 
+	/**
+	 * Maps a route to a handler that only matches if the HTTP method is DELETE
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function delete($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Maps a route to a handler that only matches if the HTTP method is OPTIONS
-		 *
-		 * @param string $routePattern
-		 * @param callable $handler
-		 * @return \Phalcon\Mvc\Micro\CollectionInterface
-		 */
-		public function options($routePattern, $handler){ }
+		$this->addToMap('DELETE', $routePattern, $handler);
 
+		return $this;
+	}
+
+	/**
+	 * Maps a route to a handler that only matches if the HTTP method is OPTIONS
+	 *
+	 * @param string $routePattern
+	 * @param callable $handler
+	 * @return \Phalcon\Mvc\Micro\CollectionInterface
+	 * @throws Exception
+	 */
+	public function options($routePattern, $handler)
+	{
+		if(is_string($routePattern) === false ||
+			is_callable($handler) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
+
+		$this->addToMap('OPTIONS', $routePattern, $handler);
+
+		return $this;
 	}
 }

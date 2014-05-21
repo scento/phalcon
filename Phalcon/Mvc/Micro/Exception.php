fe03d6a9501da0a0dc74bac1d6483d7fbb8286d5
Wenzel Pünter <wenzel@phelix.me>
2014-05-21 15:04:00 +0200

    MVC micro exception restructure

--- Phalcon/Mvc/Micro/Exception.php
+++ Phalcon/Mvc/Micro/Exception.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+/**
+ * Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Micro;
 
-namespace Phalcon\Mvc\Micro {
-
-	/**
-	 * Phalcon\Mvc\Micro\Exception
-	 *
-	 * Exceptions thrown in Phalcon\Mvc\Micro will use this class
-	 *
-	 */
+/**
+ * Phalcon\Mvc\Micro\Exception
+ *
+ * Exceptions thrown in Phalcon\Mvc\Micro will use this class
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/micro/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 	
-	class Exception extends \Phalcon\Exception {
-	}
 }

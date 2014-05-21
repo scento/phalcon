7524be6c108e6dd7e9eecdd0af29fc87237b2f1e
Wenzel Pünter <wenzel@phelix.me>
2014-05-21 15:04:52 +0200

    MVC micro middleware interface restructure

--- Phalcon/Mvc/Micro/MiddlewareInterface.php
+++ Phalcon/Mvc/Micro/MiddlewareInterface.php
@@ -1,19 +1,25 @@
-<?php 
+<?php
+/**
+ * Middleware Interfce
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Micro;
 
-namespace Phalcon\Mvc\Micro {
-
+/**
+ * Phalcon\Mvc\Micro\MiddlewareInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/micro/middlewareinterface.c
+ */
+interface MiddlewareInterface
+{
 	/**
-	 * Phalcon\Mvc\Micro\MiddlewareInterface initializer
+	 * Calls the middleware
+	 *
+	 * @param \Phalcon\Mvc\Micro $application
 	 */
-	
-	interface MiddlewareInterface {
-
-		/**
-		 * Calls the middleware
-		 *
-		 * @param \Phalcon\Mvc\Micro $application
-		 */
-		public function call($application);
-
-	}
+	public function call($application);
 }

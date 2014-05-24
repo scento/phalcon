7a6fe12893f9ccdc0d8d63951e7d3e711e221254
Wenzel Pünter <wenzel@phelix.me>
2014-05-24 11:07:37 +0200

    MVC view engine interface restructure

--- Phalcon/Mvc/View/EngineInterface.php
+++ Phalcon/Mvc/View/EngineInterface.php
@@ -1,47 +1,50 @@
-<?php 
+<?php
+/**
+ * View
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\View;
 
-namespace Phalcon\Mvc\View {
+/**
+ * Phalcon\Mvc\View\EngineInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/view/engineinterface.c
+ */
+interface EngineInterface
+{
+	/**
+	 * \Phalcon\Mvc\View\Engine constructor
+	 *
+	 * @param \Phalcon\Mvc\ViewInterface $view
+	 * @param \Phalcon\DiInterface|null $dependencyInjector
+	 */
+	public function __construct($view, $dependencyInjector = null);
 
 	/**
-	 * Phalcon\Mvc\View\EngineInterface initializer
+	 * Returns cached ouput on another view stage
+	 *
+	 * @return array
 	 */
-	
-	interface EngineInterface {
+	public function getContent();
 
-		/**
-		 * \Phalcon\Mvc\View\Engine constructor
-		 *
-		 * @param \Phalcon\Mvc\ViewInterface $view
-		 * @param \Phalcon\DiInterface $dependencyInjector
-		 */
-		public function __construct($view, $dependencyInjector=null);
+	/**
+	 * Renders a partial inside another view
+	 *
+	 * @param string $partialPath
+	 * @return string
+	 */
+	public function partial($partialPath);
 
-
-		/**
-		 * Returns cached ouput on another view stage
-		 *
-		 * @return array
-		 */
-		public function getContent();
-
-
-		/**
-		 * Renders a partial inside another view
-		 *
-		 * @param string $partialPath
-		 * @return string
-		 */
-		public function partial($partialPath);
-
-
-		/**
-		 * Renders a view using the template engine
-		 *
-		 * @param string $path
-		 * @param array $params
-		 * @param boolean $mustClean
-		 */
-		public function render($path, $params, $mustClean=null);
-
-	}
+	/**
+	 * Renders a view using the template engine
+	 *
+	 * @param string $path
+	 * @param array $params
+	 * @param boolean|null $mustClean
+	 */
+	public function render($path, $params, $mustClean = null);
 }

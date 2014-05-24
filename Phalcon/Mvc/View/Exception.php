0fae64eecccec1e1f6225a9fab70cebeb87259f1
Wenzel Pünter <wenzel@phelix.me>
2014-05-24 11:08:35 +0200

    MVC view exception restructure

--- Phalcon/Mvc/View/Exception.php
+++ Phalcon/Mvc/View/Exception.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
+/**
+ * Exception
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\View;
 
-namespace Phalcon\Mvc\View {
+/**
+ * Phalcon\Mvc\View\Exception
+ *
+ * Class for exceptions thrown by Phalcon\Mvc\View
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/view/exception.c
+ */
+class Exception extends \Phalcon\Exception
+{
 
-	/**
-	 * Phalcon\Mvc\View\Exception
-	 *
-	 * Class for exceptions thrown by Phalcon\Mvc\View
-	 */
-	
-	class Exception extends \Phalcon\Exception {
-	}
 }

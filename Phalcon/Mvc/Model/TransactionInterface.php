046921d442ea594a6c15ce981a3a07e8c162387b
Wenzel Pünter <wenzel@phelix.me>
2014-05-25 19:32:47 +0200

    MVC transaction interface restructure

--- Phalcon/Mvc/Model/TransactionInterface.php
+++ Phalcon/Mvc/Model/TransactionInterface.php
@@ -1,111 +1,106 @@
-<?php 
+<?php
+/**
+ * Transaction Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model;
 
-namespace Phalcon\Mvc\Model {
+/**
+ * Phalcon\Mvc\Model\TransactionInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/transactioninterface.c
+ */
+interface TransactionInterface
+{
+	/**
+	 * \Phalcon\Mvc\Model\Transaction constructor
+	 *
+	 * @param \Phalcon\DiInterface $dependencyInjector
+	 * @param boolean|null $autoBegin
+	 * @param string|null $service
+	 */
+	public function __construct($dependencyInjector, $autoBegin = null, $service = null);
 
 	/**
-	 * Phalcon\Mvc\Model\TransactionInterface initializer
+	 * Sets transaction manager related to the transaction
+	 *
+	 * @param \Phalcon\Mvc\Model\Transaction\ManagerInterface $manager
 	 */
-	
-	interface TransactionInterface {
+	public function setTransactionManager($manager);
 
-		/**
-		 * \Phalcon\Mvc\Model\Transaction constructor
-		 *
-		 * @param \Phalcon\DiInterface $dependencyInjector
-		 * @param boolean $autoBegin
-		 * @param string $service
-		 */
-		public function __construct($dependencyInjector, $autoBegin=null, $service=null);
+	/**
+	 * Starts the transaction
+	 *
+	 * @return boolean
+	 */
+	public function begin();
 
+	/**
+	 * Commits the transaction
+	 *
+	 * @return boolean
+	 */
+	public function commit();
 
-		/**
-		 * Sets transaction manager related to the transaction
-		 *
-		 * @param \Phalcon\Mvc\Model\Transaction\ManagerInterface $manager
-		 */
-		public function setTransactionManager($manager);
+	/**
+	 * Rollbacks the transaction
+	 *
+	 * @param string|null $rollbackMessage
+	 * @param \Phalcon\Mvc\ModelInterface|null $rollbackRecord
+	 * @return boolean
+	 */
+	public function rollback($rollbackMessage = null, $rollbackRecord = null);
 
+	/**
+	 * Returns connection related to transaction
+	 *
+	 * @return string
+	 */
+	public function getConnection();
 
-		/**
-		 * Starts the transaction
-		 *
-		 * @return boolean
-		 */
-		public function begin();
+	/**
+	 * Sets if is a reused transaction or new once
+	 *
+	 * @param boolean $isNew
+	 */
+	public function setIsNewTransaction($isNew);
 
+	/**
+	 * Sets flag to rollback on abort the HTTP connection
+	 *
+	 * @param boolean $rollbackOnAbort
+	 */
+	public function setRollbackOnAbort($rollbackOnAbort);
 
-		/**
-		 * Commits the transaction
-		 *
-		 * @return boolean
-		 */
-		public function commit();
+	/**
+	 * Checks whether transaction is managed by a transaction manager
+	 *
+	 * @return boolean
+	 */
+	public function isManaged();
 
+	/**
+	 * Returns validations messages from last save try
+	 *
+	 * @return array
+	 */
+	public function getMessages();
 
-		/**
-		 * Rollbacks the transaction
-		 *
-		 * @param  string $rollbackMessage
-		 * @param  \Phalcon\Mvc\ModelInterface $rollbackRecord
-		 * @return boolean
-		 */
-		public function rollback($rollbackMessage=null, $rollbackRecord=null);
+	/**
+	 * Checks whether internal connection is under an active transaction
+	 *
+	 * @return boolean
+	 */
+	public function isValid();
 
-
-		/**
-		 * Returns connection related to transaction
-		 *
-		 * @return string
-		 */
-		public function getConnection();
-
-
-		/**
-		 * Sets if is a reused transaction or new once
-		 *
-		 * @param boolean $isNew
-		 */
-		public function setIsNewTransaction($isNew);
-
-
-		/**
-		 * Sets flag to rollback on abort the HTTP connection
-		 *
-		 * @param boolean $rollbackOnAbort
-		 */
-		public function setRollbackOnAbort($rollbackOnAbort);
-
-
-		/**
-		 * Checks whether transaction is managed by a transaction manager
-		 *
-		 * @return boolean
-		 */
-		public function isManaged();
-
-
-		/**
-		 * Returns validations messages from last save try
-		 *
-		 * @return array
-		 */
-		public function getMessages();
-
-
-		/**
-		 * Checks whether internal connection is under an active transaction
-		 *
-		 * @return boolean
-		 */
-		public function isValid();
-
-
-		/**
-		 * Sets object which generates rollback action
-		 *
-		 * @param \Phalcon\Mvc\ModelInterface $record
-		 */
-		public function setRollbackedRecord($record);
-
-	}
+	/**
+	 * Sets object which generates rollback action
+	 *
+	 * @param \Phalcon\Mvc\ModelInterface $record
+	 */
+	public function setRollbackedRecord($record);
 }

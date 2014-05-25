6351acef27c61787850b21cac403ded601f0bb9b
Wenzel Pünter <wenzel@phelix.me>
2014-05-25 19:31:20 +0200

    MVC resultset interface restructure

--- Phalcon/Mvc/Model/ResultsetInterface.php
+++ Phalcon/Mvc/Model/ResultsetInterface.php
@@ -1,68 +1,68 @@
-<?php 
+<?php
+/**
+ * Resultset Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model;
 
-namespace Phalcon\Mvc\Model {
+/**
+ * Phalcon\Mvc\Model\ResultsetInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/resultsetinterface.c
+ */
+interface ResultsetInterface
+{
+	/**
+	 * Returns the internal type of data retrieval that the resultset is using
+	 *
+	 * @return int
+	 */
+	public function getType();
 
 	/**
-	 * Phalcon\Mvc\Model\ResultsetInterface initializer
+	 * Get first row in the resultset
+	 *
+	 * @return \Phalcon\Mvc\ModelInterface
 	 */
-	
-	interface ResultsetInterface {
+	public function getFirst();
 
-		/**
-		 * Returns the internal type of data retrieval that the resultset is using
-		 *
-		 * @return int
-		 */
-		public function getType();
+	/**
+	 * Get last row in the resultset
+	 *
+	 * @return \Phalcon\Mvc\ModelInterface
+	 */
+	public function getLast();
 
+	/**
+	 * Set if the resultset is fresh or an old one cached
+	 *
+	 * @param boolean $isFresh
+	 */
+	public function setIsFresh($isFresh);
 
-		/**
-		 * Get first row in the resultset
-		 *
-		 * @return \Phalcon\Mvc\ModelInterface
-		 */
-		public function getFirst();
+	/**
+	 * Tell if the resultset if fresh or an old one cached
+	 *
+	 * @return boolean
+	 */
+	public function isFresh();
 
+	/**
+	 * Returns the associated cache for the resultset
+	 *
+	 * @return \Phalcon\Cache\BackendInterface
+	 */
+	public function getCache();
 
-		/**
-		 * Get last row in the resultset
-		 *
-		 * @return \Phalcon\Mvc\ModelInterface
-		 */
-		public function getLast();
-
-
-		/**
-		 * Set if the resultset is fresh or an old one cached
-		 *
-		 * @param boolean $isFresh
-		 */
-		public function setIsFresh($isFresh);
-
-
-		/**
-		 * Tell if the resultset if fresh or an old one cached
-		 *
-		 * @return boolean
-		 */
-		public function isFresh();
-
-
-		/**
-		 * Returns the associated cache for the resultset
-		 *
-		 * @return \Phalcon\Cache\BackendInterface
-		 */
-		public function getCache();
-
-
-		/**
-		 * Returns a complete resultset as an array, if the resultset has a big number of rows
-		 * it could consume more memory than currently it does.
-		 *
-		 * @return array
-		 */
-		public function toArray();
-
-	}
+	/**
+	 * Returns a complete resultset as an array, if the resultset has a big number of rows
+	 * it could consume more memory than currently it does.
+	 *
+	 * @return array
+	 */
+	public function toArray();
 }

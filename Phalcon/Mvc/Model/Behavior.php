5e5b1e39ff5275c184028b87677e21751e8839dc
Wenzel Pünter <wenzel@phelix.me>
2014-05-25 19:14:45 +0200

    MVC model behavior implementation

--- Phalcon/Mvc/Model/Behavior.php
+++ Phalcon/Mvc/Model/Behavior.php
@@ -1,59 +1,116 @@
-<?php 
+<?php
+/**
+ * Behavior
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model;
 
-namespace Phalcon\Mvc\Model {
+use \Phalcon\Mvc\Model\Exception;
+
+/**
+ * Phalcon\Mvc\Model\Behavior
+ *
+ * This is an optional base class for ORM behaviors
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/behavior.c
+ */
+abstract class Behavior
+{
+	/**
+	 * Options
+	 * 
+	 * @var null|array
+	 * @access protected
+	*/
+	protected $_options;
 
 	/**
-	 * Phalcon\Mvc\Model\Behavior
+	 * \Phalcon\Mvc\Model\Behavior
 	 *
-	 * This is an optional base class for ORM behaviors
+	 * @param array|null $options
+	 * @throws Exception
 	 */
-	
-	abstract class Behavior {
+	public function __construct($options = null)
+	{
+		if(is_null($options) === false &&
+			is_array($options) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_options;
+		$this->_options = $options;
+	}
 
-		/**
-		 * \Phalcon\Mvc\Model\Behavior
-		 *
-		 * @param array $options
-		 */
-		public function __construct($options=null){ }
+	/**
+	 * Checks whether the behavior must take action on certain event
+	 *
+	 * @param string $eventName
+	 * @return boolean
+	 * @throws Exception
+	 */
+	protected function mustTakeAction($eventName)
+	{
+		if(is_string($eventName) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
+		if(is_array($this->_options) === false) {
+			return false;
+		}
 
-		/**
-		 * Checks whether the behavior must take action on certain event
-		 *
-		 * @param string $eventName
-		 */
-		protected function mustTakeAction(){ }
+		return isset($this->_options[$eventName]);
+	}
 
+	/**
+	 * Returns the behavior options related to an event
+	 *
+	 * @param string|null $eventName
+	 * @return array
+	 * @throws Exception
+	 */
+	protected function getOptions($eventName = null)
+	{
+		if(is_string($eventName) === false &&
+			is_null($eventName) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Returns the behavior options related to an event
-		 *
-		 * @param string $eventName
-		 * @return array
-		 */
-		protected function getOptions(){ }
+		if(is_null($eventName) === false) {
+			if(isset($this->_options[$eventName]) === true) {
+				return $this->_options[$eventName];
+			}
 
+			return null;
+		}
 
-		/**
-		 * This method receives the notifications from the EventsManager
-		 *
-		 * @param string $type
-		 * @param \Phalcon\Mvc\ModelInterface $model
-		 */
-		public function notify($type, $model){ }
+		return $this->_options;
+	}
 
+	/**
+	 * This method receives the notifications from the EventsManager
+	 *
+	 * @param string $type
+	 * @param \Phalcon\Mvc\ModelInterface $model
+	 */
+	public function notify($type, $model)
+	{
 
-		/**
-		 * Acts as fallbacks when a missing method is called on the model
-		 *
-		 * @param \Phalcon\Mvc\ModelInterface $model
-		 * @param string $method
-		 * @param array $arguments
-		 */
-		public function missingMethod($model, $method, $arguments=null){ }
+	}
 
+	/**
+	 * Acts as fallbacks when a missing method is called on the model
+	 *
+	 * @param \Phalcon\Mvc\ModelInterface $model
+	 * @param string $method
+	 * @param array|null $arguments
+	 * @throws Exception
+	 */
+	public function missingMethod($model, $method, $arguments = null)
+	{
+		
 	}
 }

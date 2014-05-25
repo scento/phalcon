3e451a5329d97190e71c5d3b7a72a6402f413129
Wenzel Pünter <wenzel@phelix.me>
2014-05-25 19:30:10 +0200

    MVC result interface restructure

--- Phalcon/Mvc/Model/ResultInterface.php
+++ Phalcon/Mvc/Model/ResultInterface.php
@@ -1,19 +1,25 @@
-<?php 
+<?php
+/**
+ * Result Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model;
 
-namespace Phalcon\Mvc\Model {
-
+/**
+ * Phalcon\Mvc\Model\ResultInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/resultinterface.c
+ */
+interface ResultInterface
+{
 	/**
-	 * Phalcon\Mvc\Model\ResultInterface initializer
+	 * Sets the object's state
+	 *
+	 * @param boolean $dirtyState
 	 */
-	
-	interface ResultInterface {
-
-		/**
-		 * Sets the object's state
-		 *
-		 * @param boolean $dirtyState
-		 */
-		public function setDirtyState($dirtyState);
-
-	}
+	public function setDirtyState($dirtyState);
 }

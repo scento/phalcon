3e2dd7ea47029f2c124571f4aac8f48e44be2198
Wenzel Pünter <wenzel@phelix.me>
2014-05-31 16:52:50 +0200

    MVC validation failed implementation

--- Phalcon/Mvc/Model/ValidationFailed.php
+++ Phalcon/Mvc/Model/ValidationFailed.php
@@ -1,43 +1,83 @@
-<?php 
+<?php
+/**
+ * Validation Failed
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model;
 
-namespace Phalcon\Mvc\Model {
+use \Phalcon\Mvc\Model\Exception,
+	\Phalcon\Mvc\Model;
+
+/**
+ * Phalcon\Mvc\Model\ValidationFailed
+ *
+ * This exception is generated when a model fails to save a record
+ * Phalcon\Mvc\Model must be set up to have this behavior
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/validationfailed.c
+ */
+class ValidationFailed extends Exception
+{
+	/**
+	 * Model
+	 * 
+	 * @var null|\Phalcon\Mvc\Model
+	 * @access protected
+	*/
+	protected $_model;
 
 	/**
-	 * Phalcon\Mvc\Model\ValidationFailed
+	 * Messages
+	 * 
+	 * @var null|array
+	 * @access protected
+	*/
+	protected $_messages;
+
+	/**
+	 * \Phalcon\Mvc\Model\ValidationFailed constructor
 	 *
-	 * This exception is generated when a model fails to save a record
-	 * Phalcon\Mvc\Model must be set up to have this behavior
+	 * @param \Phalcon\Mvc\Model $model
+	 * @param \Phalcon\Mvc\Model\Message[] $validationMessages
+	 * @throws Exception
 	 */
-	
-	class ValidationFailed extends \Phalcon\Mvc\Model\Exception {
+	public function __construct($model, $validationMessages)
+	{
+		if(is_object($model) === false ||
+			$model instanceof Model === false ||
+			is_array($validationMessages) === false ||
+			empty($validationMessages) === true) {
+			throw new Exception('Validation failed');
+		}
 
-		protected $_model;
+		$message_str = $validationMessages[0]->getMessage();
+		$this->_model = $model;
+		$this->_messages = $validationMessages;
+		parent::__construct($validationMessages[0]);
+	}
 
-		protected $_messages;
+	/**
+	 * Returns the complete group of messages produced in the validation
+	 *
+	 * @return \Phalcon\Mvc\Model\Message[]|null
+	 */
+	public function getMessages()
+	{
+		return $this->_messages;
+	}
 
-		/**
-		 * \Phalcon\Mvc\Model\ValidationFailed constructor
-		 *
-		 * @param \Phalcon\Mvc\Model $model
-		 * @param \Phalcon\Mvc\Model\Message[] $validationMessages
-		 */
-		public function __construct($model, $validationMessages){ }
-
-
-		/**
-		 * Returns the complete group of messages produced in the validation
-		 *
-		 * @return \Phalcon\Mvc\Model\Message[]
-		 */
-		public function getMessages(){ }
-
-
-		/**
-		 * Returns the model that generated the messages
-		 *
-		 * @return \Phalcon\Mvc\Model
-		 */
-		public function getModel(){ }
-
+	/**
+	 * Returns the model that generated the messages
+	 *
+	 * @return \Phalcon\Mvc\Model|null
+	 */
+	public function getModel()
+	{
+		return $this->_model;
 	}
 }

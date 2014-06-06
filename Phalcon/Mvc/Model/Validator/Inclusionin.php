456f4e096945cd6e4881ca623a0c9f94966482e7
Wenzel Pünter <wenzel@phelix.me>
2014-06-06 19:58:07 +0200

    MVC inclusion validator implementation

--- Phalcon/Mvc/Model/Validator/Inclusionin.php
+++ Phalcon/Mvc/Model/Validator/Inclusionin.php
@@ -1,42 +1,87 @@
-<?php 
+<?php
+/**
+ * Inclusion In Validator
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model\Validator;
 
-namespace Phalcon\Mvc\Model\Validator {
+use \Phalcon\Mvc\Model\Validator,
+	\Phalcon\Mvc\Model\ValidatorInterface,
+	\Phalcon\Mvc\Model\Exception,
+	\Phalcon\Mvc\ModelInterface;
 
+/**
+ * Phalcon\Mvc\Model\Validator\InclusionIn
+ *
+ * Check if a value is included into a list of values
+ *
+ *<code>
+ *	use Phalcon\Mvc\Model\Validator\InclusionIn as InclusionInValidator;
+ *
+ *	class Subscriptors extends Phalcon\Mvc\Model
+ *	{
+ *
+ *		public function validation()
+ *		{
+ *			$this->validate(new InclusionInValidator(array(
+ *				'field' => 'status',
+ *				'domain' => array('A', 'I')
+ *			)));
+ *			if ($this->validationHasFailed() == true) {
+ *				return false;
+ *			}
+ *		}
+ *
+ *	}
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/validator/inclusionin.c
+ */
+class Inclusionin extends Validator implements ValidatorInterface
+{
 	/**
-	 * Phalcon\Mvc\Model\Validator\InclusionIn
+	 * Executes validator
 	 *
-	 * Check if a value is included into a list of values
-	 *
-	 *<code>
-	 *	use Phalcon\Mvc\Model\Validator\InclusionIn as InclusionInValidator;
-	 *
-	 *	class Subscriptors extends Phalcon\Mvc\Model
-	 *	{
-	 *
-	 *		public function validation()
-	 *		{
-	 *			$this->validate(new InclusionInValidator(array(
-	 *				'field' => 'status',
-	 *				'domain' => array('A', 'I')
-	 *			)));
-	 *			if ($this->validationHasFailed() == true) {
-	 *				return false;
-	 *			}
-	 *		}
-	 *
-	 *	}
-	 *</code>
+	 * @param \Phalcon\Mvc\ModelInterface $record
+	 * @return boolean
+	 * @throws Exception
 	 */
-	
-	class Inclusionin extends \Phalcon\Mvc\Model\Validator implements \Phalcon\Mvc\Model\ValidatorInterface {
+	public function validate($record)
+	{
+		$field_name = $this->getOption('field');
+		if(is_string($field_name) === false) {
+			throw new Exception('Field name must be a string');
+		}
 
-		/**
-		 * Executes validator
-		 *
-		 * @param \Phalcon\Mvc\ModelInterface $record
-		 * @return boolean
-		 */
-		public function validate($record){ }
+		//The 'domain' option must be a valid array of not allowed values
+		if($this->isSetOption('domain') === false) {
+			throw new Exception("The option 'domain' is required for this validator");
+		}
 
+		$domain = $this->getOption('domain');
+		if(is_array($domain) === false) {
+			throw new Exception("Option 'domain' must be an array");
+		}
+
+		$value = $record->readAttribute($field_name);
+
+		//We check if the value is contained in the array using "in_array"
+		if(in_array($value, $domain) === false) {
+			//Check if the developer has defined a custom message
+			$message = $this->getOption('message');
+			if(isset($message) === false) {
+				$message = "Value of field '".$field_name."' must not be part of list: ".implode(', ', $domain);
+			}
+
+			$this->appendMessage($message, $field_name, 'Inclusion');
+			return false;
+		}
+
+		return true;
 	}
 }

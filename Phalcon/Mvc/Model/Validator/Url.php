521e4076c299998c44418272534f7a983ee3b685
Wenzel Pünter <wenzel@phelix.me>
2014-06-06 21:46:49 +0200

    MVC url validator implementation

--- Phalcon/Mvc/Model/Validator/Url.php
+++ Phalcon/Mvc/Model/Validator/Url.php
@@ -1,42 +1,82 @@
-<?php 
+<?php
+/**
+ * URL Validator
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model\Validator;
 
-namespace Phalcon\Mvc\Model\Validator {
+use \Phalcon\Mvc\Model\Validator,
+	\Phalcon\Mvc\Model\ValidatorInterface,
+	\Phalcon\Mvc\Model\Exception,
+	\Phalcon\Mvc\ModelInterface;
 
+/**
+ * Phalcon\Mvc\Model\Validator\Url
+ *
+ * Allows to validate if a field has a url format
+ *
+ *<code>
+ *use Phalcon\Mvc\Model\Validator\Url as UrlValidator;
+ *
+ *class Posts extends Phalcon\Mvc\Model
+ *{
+ *
+ *  public function validation()
+ *  {
+ *      $this->validate(new UrlValidator(array(
+ *          'field' => 'source_url'
+ *      )));
+ *      if ($this->validationHasFailed() == true) {
+ *          return false;
+ *      }
+ *  }
+ *
+ *}
+ *</code>
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/validator/url.c
+ */
+class Url extends Validator implements ValidatorInterface
+{
 	/**
-	 * Phalcon\Mvc\Model\Validator\Url
+	 * Executes the validator
 	 *
-	 * Allows to validate if a field has a url format
-	 *
-	 *<code>
-	 *use Phalcon\Mvc\Model\Validator\Url as UrlValidator;
-	 *
-	 *class Posts extends Phalcon\Mvc\Model
-	 *{
-	 *
-	 *  public function validation()
-	 *  {
-	 *      $this->validate(new UrlValidator(array(
-	 *          'field' => 'source_url'
-	 *      )));
-	 *      if ($this->validationHasFailed() == true) {
-	 *          return false;
-	 *      }
-	 *  }
-	 *
-	 *}
-	 *</code>
-	 *
+	 * @param \Phalcon\Mvc\ModelInterface $record
+	 * @return boolean
+	 * @throws Exception
 	 */
-	
-	class Url extends \Phalcon\Mvc\Model\Validator implements \Phalcon\Mvc\Model\ValidatorInterface {
+	public function validate($record)
+	{
+		if(is_object($record) === false ||
+			$record instanceof ModelInterface === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Executes the validator
-		 *
-		 * @param \Phalcon\Mvc\ModelInterface $record
-		 * @return boolean
-		 */
-		public function validate($record){ }
+		$field = $this->getOption('field');
+		if(is_string($field) === false) {
+			throw new Exception('Field name must be a string');
+		}
 
+		$value = $record->readAttribute($field);
+
+		//Filters the format using FILTER_VALIDATE_URL
+		if(filter_var($value, 273) === false) {
+			//Check if the developer has defined a custom message
+			$message = $this->getOption('message');
+
+			if(isset($message) === false) {
+				$message = "'".$field."' does not have a valid url format";
+			}
+
+			$this->appendMessage($message, $field, 'Url');
+			return false;
+		}
+
+		return true;
 	}
 }

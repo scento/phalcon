ec3369f81e48f109a6afd9c5359e5238043a5f6b
Wenzel Pünter <wenzel@phelix.me>
2014-06-20 20:40:50 +0200

    MVC model transaction interface restructure

--- Phalcon/Mvc/Model/Transaction/ManagerInterface.php
+++ Phalcon/Mvc/Model/Transaction/ManagerInterface.php
@@ -1,82 +1,77 @@
-<?php 
+<?php
+/**
+* Manager Interface
+*
+* @author Andres Gutierrez <andres@phalconphp.com>
+* @author Eduar Carvajal <eduar@phalconphp.com>
+* @version 1.2.6
+* @package Phalcon
+*/
+namespace Phalcon\Mvc\Model\Transaction;
 
-namespace Phalcon\Mvc\Model\Transaction {
+/**
+ * Phalcon\Mvc\Model\Transaction\ManagerInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/transaction/managerinterface.c
+ */
+interface ManagerInterface
+{
+	/**
+	 * \Phalcon\Mvc\Model\Transaction\Manager
+	 *
+	 * @param \Phalcon\DiInterface|null $dependencyInjector
+	 */
+	public function __construct($dependencyInjector = null);
 
 	/**
-	 * Phalcon\Mvc\Model\Transaction\ManagerInterface initializer
+	 * Checks whether manager has an active transaction
+	 *
+	 * @return boolean
 	 */
-	
-	interface ManagerInterface {
+	public function has();
 
-		/**
-		 * \Phalcon\Mvc\Model\Transaction\Manager
-		 *
-		 * @param \Phalcon\DiInterface $dependencyInjector
-		 */
-		public function __construct($dependencyInjector=null);
+	/**
+	 * Returns a new \Phalcon\Mvc\Model\Transaction or an already created once
+	 *
+	 * @param boolean|null $autoBegin
+	 * @return \Phalcon\Mvc\Model\TransactionInterface
+	 */
+	public function get($autoBegin = null);
 
+	/**
+	 * Rollbacks active transactions within the manager
+	 */
+	public function rollbackPendent();
 
-		/**
-		 * Checks whether manager has an active transaction
-		 *
-		 * @return boolean
-		 */
-		public function has();
+	/**
+	 * Commmits active transactions within the manager
+	 */
+	public function commit();
 
+	/**
+	 * Rollbacks active transactions within the manager
+	 * Collect will remove transaction from the manager
+	 *
+	 * @param boolean|null $collect
+	 */
+	public function rollback($collect = null);
 
-		/**
-		 * Returns a new \Phalcon\Mvc\Model\Transaction or an already created once
-		 *
-		 * @param boolean $autoBegin
-		 * @return \Phalcon\Mvc\Model\TransactionInterface
-		 */
-		public function get($autoBegin=null);
+	/**
+	 * Notifies the manager about a rollbacked transaction
+	 *
+	 * @param \Phalcon\Mvc\Model\TransactionInterface $transaction
+	 */
+	public function notifyRollback($transaction);
 
+	/**
+	 * Notifies the manager about a commited transaction
+	 *
+	 * @param \Phalcon\Mvc\Model\TransactionInterface $transaction
+	 */
+	public function notifyCommit($transaction);
 
-		/**
-		 * Rollbacks active transactions within the manager
-		 *
-		 */
-		public function rollbackPendent();
-
-
-		/**
-		 * Commmits active transactions within the manager
-		 *
-		 */
-		public function commit();
-
-
-		/**
-		 * Rollbacks active transactions within the manager
-		 * Collect will remove transaction from the manager
-		 *
-		 * @param boolean $collect
-		 */
-		public function rollback($collect=null);
-
-
-		/**
-		 * Notifies the manager about a rollbacked transaction
-		 *
-		 * @param \Phalcon\Mvc\Model\TransactionInterface $transaction
-		 */
-		public function notifyRollback($transaction);
-
-
-		/**
-		 * Notifies the manager about a commited transaction
-		 *
-		 * @param \Phalcon\Mvc\Model\TransactionInterface $transaction
-		 */
-		public function notifyCommit($transaction);
-
-
-		/**
-		 * Remove all the transactions from the manager
-		 *
-		 */
-		public function collectTransactions();
-
-	}
+	/**
+	 * Remove all the transactions from the manager
+	 */
+	public function collectTransactions();
 }

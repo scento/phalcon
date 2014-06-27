b13626e929c23a75c4c7471d36acf63a6e5fbc53
Wenzel Pünter <wenzel@phelix.me>
2014-06-27 19:12:09 +0200

    MVC model query status interface restructure

--- src/Phalcon/Mvc/Model/Query/StatusInterface.php
+++ src/Phalcon/Mvc/Model/Query/StatusInterface.php
@@ -1,44 +1,47 @@
-<?php 
+<?php
+/**
+ * Status Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\Model\Query;
 
-namespace Phalcon\Mvc\Model\Query {
+/**
+ * Phalcon\Mvc\Model\Query\StatusInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/mvc/model/query/statusinterface.c
+ */
+interface StatusInterface
+{
+	/**
+	 * \Phalcon\Mvc\Model\Query\Status
+	 *
+	 * @param boolean $success
+	 * @param \Phalcon\Mvc\ModelInterface $model
+	 */
+	public function __construct($success, $model);
 
 	/**
-	 * Phalcon\Mvc\Model\Query\StatusInterface initializer
+	 * Returns the model which executed the action
+	 *
+	 * @return \Phalcon\Mvc\ModelInterface
 	 */
-	
-	interface StatusInterface {
+	public function getModel();
 
-		/**
-		 * \Phalcon\Mvc\Model\Query\Status
-		 *
-		 * @param boolean $success
-		 * @param \Phalcon\Mvc\ModelInterface $model
-		 */
-		public function __construct($success, $model);
+	/**
+	 * Returns the messages produced by a operation failed
+	 *
+	 * @return \Phalcon\Mvc\Model\MessageInterface[]
+	 */
+	public function getMessages();
 
-
-		/**
-		 * Returns the model which executed the action
-		 *
-		 * @return \Phalcon\Mvc\ModelInterface
-		 */
-		public function getModel();
-
-
-		/**
-		 * Returns the messages produced by a operation failed
-		 *
-		 * @return \Phalcon\Mvc\Model\MessageInterface[]
-		 */
-		public function getMessages();
-
-
-		/**
-		 * Allows to check if the executed operation was successful
-		 *
-		 * @return boolean
-		 */
-		public function success();
-
-	}
+	/**
+	 * Allows to check if the executed operation was successful
+	 *
+	 * @return boolean
+	 */
+	public function success();
 }

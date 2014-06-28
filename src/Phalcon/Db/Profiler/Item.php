b779d26264be4bc1ba2fd4de7d39a512d37eeb5f
Wenzel Pünter <wenzel@phelix.me>
2014-06-28 15:22:36 +0200

    Database profiler item implementation

--- src/Phalcon/Db/Profiler/Item.php
+++ src/Phalcon/Db/Profiler/Item.php
@@ -1,76 +1,132 @@
-<?php 
+<?php
+/**
+ * Item
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Db\Profiler;
 
-namespace Phalcon\Db\Profiler {
+use \Phalcon\Db\Exception;
+
+/**
+ * Phalcon\Db\Profiler\Item
+ *
+ * This class identifies each profile in a Phalcon\Db\Profiler
+ *
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/db/profiler/item.c
+ */
+class Item
+{
+	/**
+	 * SQL Statement
+	 * 
+	 * @var null|string
+	 * @access protected
+	*/
+	protected $_sqlStatement;
 
 	/**
-	 * Phalcon\Db\Profiler\Item
+	 * Initial Time
+	 * 
+	 * @var null|int
+	 * @access protected
+	*/
+	protected $_initialTime;
+
+	/**
+	 * Final Time
+	 * 
+	 * @var null|int
+	 * @access protected
+	*/
+	protected $_finalTime;
+
+	/**
+	 * Sets the SQL statement related to the profile
 	 *
-	 * This class identifies each profile in a Phalcon\Db\Profiler
-	 *
+	 * @param string $sqlStatement
+	 * @throws Exception
 	 */
-	
-	class Item {
+	public function setSQLStatement($sqlStatement)
+	{
+		if(is_string($sqlStatement) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_sqlStatement;
+		$this->_sqlStatement = $sqlStatement;
+	}
 
-		protected $_initialTime;
+	/**
+	 * Returns the SQL statement related to the profile
+	 *
+	 * @return string|null
+	 */
+	public function getSQLStatement()
+	{
+		return $this->_sqlStatement;
+	}
 
-		protected $_finalTime;
+	/**
+	 * Sets the timestamp on when the profile started
+	 *
+	 * @param int $initialTime
+	 * @throws Exception
+	 */
+	public function setInitialTime($initialTime)
+	{
+		if(is_int($initialTime) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Sets the SQL statement related to the profile
-		 *
-		 * @param string $sqlStatement
-		 */
-		public function setSQLStatement($sqlStatement){ }
+		$this->_initialTime = $initialTime;
+	}
 
+	/**
+	 * Sets the timestamp on when the profile ended
+	 *
+	 * @param int $finalTime
+	 * @throws Exception
+	 */
+	public function setFinalTime($finalTime)
+	{
+		if(is_int($finalTime) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Returns the SQL statement related to the profile
-		 *
-		 * @return string
-		 */
-		public function getSQLStatement(){ }
+		$this->_finalTime = $finalTime;
+	}
 
+	/**
+	 * Returns the initial time in milseconds on when the profile started
+	 *
+	 * @return int|null
+	 */
+	public function getInitialTime()
+	{
+		return $this->_initialTime;
+	}
 
-		/**
-		 * Sets the timestamp on when the profile started
-		 *
-		 * @param int $initialTime
-		 */
-		public function setInitialTime($initialTime){ }
+	/**
+	 * Returns the initial time in milseconds on when the profile ended
+	 *
+	 * @return int|null
+	 */
+	public function getFinalTime()
+	{
+		return $this->_finalTime;
+	}
 
-
-		/**
-		 * Sets the timestamp on when the profile ended
-		 *
-		 * @param int $finalTime
-		 */
-		public function setFinalTime($finalTime){ }
-
-
-		/**
-		 * Returns the initial time in milseconds on when the profile started
-		 *
-		 * @return double
-		 */
-		public function getInitialTime(){ }
-
-
-		/**
-		 * Returns the initial time in milseconds on when the profile ended
-		 *
-		 * @return double
-		 */
-		public function getFinalTime(){ }
-
-
-		/**
-		 * Returns the total time in seconds spent by the profile
-		 *
-		 * @return double
-		 */
-		public function getTotalElapsedSeconds(){ }
-
+	/**
+	 * Returns the total time in seconds spent by the profile
+	 *
+	 * @return int
+	 */
+	public function getTotalElapsedSeconds()
+	{
+		return $this->_finalTime - $this->_initialTime;
 	}
 }

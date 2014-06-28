5d8db794b72020779e29ad918d57cb3104030688
Wenzel Pünter <wenzel@phelix.me>
2014-06-28 15:13:23 +0200

    Database index implementation

--- src/Phalcon/Db/Index.php
+++ src/Phalcon/Db/Index.php
@@ -1,53 +1,103 @@
-<?php 
+<?php
+/**
+ * Index
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Db;
 
-namespace Phalcon\Db {
+use \Phalcon\Db\IndexInterface,
+	\Phalcon\Db\Exception;
+
+/**
+ * Phalcon\Db\Index
+ *
+ * Allows to define indexes to be used on tables. Indexes are a common way
+ * to enhance database performance. An index allows the database server to find
+ * and retrieve specific rows much faster than it could do without an index
+ */
+class Index implements IndexInterface
+{
+	/**
+	 * Index Name
+	 * 
+	 * @var null
+	 * @access protected
+	*/
+	protected $_indexName;
 
 	/**
-	 * Phalcon\Db\Index
+	 * Columns
+	 * 
+	 * @var null
+	 * @access protected
+	*/
+	protected $_columns;
+
+	/**
+	 * \Phalcon\Db\Index constructor
 	 *
-	 * Allows to define indexes to be used on tables. Indexes are a common way
-	 * to enhance database performance. An index allows the database server to find
-	 * and retrieve specific rows much faster than it could do without an index
+	 * @param string $indexName
+	 * @param array $columns
+	 * @throws Exception
 	 */
-	
-	class Index implements \Phalcon\Db\IndexInterface {
+	public function __construct($indexName, $columns)
+	{
+		if(is_string($indexName) === false ||
+			is_array($columns) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_indexName;
+		$this->_indexName = $indexName;
+		$this->_columns = $columns;
+	}
 
-		protected $_columns;
+	/**
+	 * Gets the index name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->_indexName;
+	}
 
-		/**
-		 * \Phalcon\Db\Index constructor
-		 *
-		 * @param string $indexName
-		 * @param array $columns
-		 */
-		public function __construct($indexName, $columns){ }
+	/**
+	 * Gets the columns that comprends the index
+	 *
+	 * @return array
+	 */
+	public function getColumns()
+	{
+		return $this->_columns;
+	}
 
+	/**
+	 * Restore a \Phalcon\Db\Index object from export
+	 *
+	 * @param array $data
+	 * @return \Phalcon\Db\IndexInterface
+	 * @throws Exception
+	 */
+	public static function __set_state($data)
+	{
+		if(is_array($data) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		/**
-		 * Gets the index name
-		 *
-		 * @return string
-		 */
-		public function getName(){ }
+		if(isset($data['_indexName']) === false) {
+			throw new Exception('_indexName parameter is required');
+		}
 
+		if(isset($data['_columns']) === false) {
+			throw new Exception('_columns parameter is required');
+		}
 
-		/**
-		 * Gets the columns that comprends the index
-		 *
-		 * @return array
-		 */
-		public function getColumns(){ }
-
-
-		/**
-		 * Restore a \Phalcon\Db\Index object from export
-		 *
-		 * @param array $data
-		 * @return \Phalcon\Db\IndexInterface
-		 */
-		public static function __set_state($data){ }
-
+		//Return a Phalcon\Db\Index as part of the returning state
+		return new Index($data['_indexName'], $data['_columns']);
 	}
 }

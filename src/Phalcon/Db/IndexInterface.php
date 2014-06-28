3e0839e6063e7abd688988ba5bdf758bf9d54d02
Wenzel Pünter <wenzel@phelix.me>
2014-06-28 11:29:58 +0200

    Database index interface restructure

--- src/Phalcon/Db/IndexInterface.php
+++ src/Phalcon/Db/IndexInterface.php
@@ -1,45 +1,48 @@
-<?php 
+<?php
+/**
+ * Index Interface
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Db;
 
-namespace Phalcon\Db {
+/**
+ * Phalcon\Db\IndexInterface initializer
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/db/indexinterface.c
+ */
+interface IndexInterface
+{
+	/**
+	 * \Phalcon\Db\Index constructor
+	 *
+	 * @param string $indexName
+	 * @param array $columns
+	 */
+	public function __construct($indexName, $columns);
 
 	/**
-	 * Phalcon\Db\IndexInterface initializer
+	 * Gets the index name
+	 *
+	 * @return string
 	 */
-	
-	interface IndexInterface {
+	public function getName();
 
-		/**
-		 * \Phalcon\Db\Index constructor
-		 *
-		 * @param string $indexName
-		 * @param array $columns
-		 */
-		public function __construct($indexName, $columns);
+	/**
+	 * Gets the columns that comprends the index
+	 *
+	 * @return array
+	 */
+	public function getColumns();
 
-
-		/**
-		 * Gets the index name
-		 *
-		 * @return string
-		 */
-		public function getName();
-
-
-		/**
-		 * Gets the columns that comprends the index
-		 *
-		 * @return array
-		 */
-		public function getColumns();
-
-
-		/**
-		 * Restore a \Phalcon\Db\Index object from export
-		 *
-		 * @param array $data
-		 * @return \Phalcon\Db\IndexInterface
-		 */
-		public static function __set_state($data);
-
-	}
+	/**
+	 * Restore a \Phalcon\Db\Index object from export
+	 *
+	 * @param array $data
+	 * @return \Phalcon\Db\IndexInterface
+	 */
+	public static function __set_state($data);
 }

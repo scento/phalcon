5f5f93062af7eb33480aff10b51a2f096da5610c
Wenzel Pünter <wenzel@phelix.me>
2014-07-07 19:28:12 +0200

    Mysql database adapter implementation

--- src/Phalcon/Db/Adapter/Pdo/Mysql.php
+++ src/Phalcon/Db/Adapter/Pdo/Mysql.php
@@ -1,54 +1,266 @@
-<?php 
+<?php
+/**
+ * Mysql
+ *
+ * @author Andres Gutierrez <andres@phalconphp.com>
+ * @author Eduar Carvajal <eduar@phalconphp.com>
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Db\Adapter\Pdo;
 
-namespace Phalcon\Db\Adapter\Pdo {
+use \Phalcon\Db\Adapter\Pdo,
+	\Phalcon\Db\Column,
+	\Phalcon\Db\AdapterInterface,
+	\Phalcon\Db\Exception,
+	\Phalcon\Events\EventsAwareInterface;
+
+/**
+ * Phalcon\Db\Adapter\Pdo\Mysql
+ *
+ * Specific functions for the Mysql database system
+ *
+ *<code>
+ *
+ *	$config = array(
+ *		"host" => "192.168.0.11",
+ *		"dbname" => "blog",
+ *		"port" => 3306,
+ *		"username" => "sigma",
+ *		"password" => "secret"
+ *	);
+ *
+ *	$connection = new Phalcon\Db\Adapter\Pdo\Mysql($config);
+ *
+ *</code>
+ * 
+ * @see https://github.com/phalcon/cphalcon/blob/1.2.6/ext/db/adapter/pdo/mysql.c
+ */
+class Mysql extends Pdo implements EventsAwareInterface, AdapterInterface
+{
+	/**
+	 * Type
+	 * 
+	 * @var string
+	 * @access protected
+	*/
+	protected $_type = 'mysql';
 
 	/**
-	 * Phalcon\Db\Adapter\Pdo\Mysql
+	 * Dialect Type
+	 * 
+	 * @var string
+	 * @access protected
+	*/
+	protected $_dialectType = 'mysql';
+
+	/**
+	 * Escapes a column/table/schema name
 	 *
-	 * Specific functions for the Mysql database system
-	 *
-	 *<code>
-	 *
-	 *	$config = array(
-	 *		"host" => "192.168.0.11",
-	 *		"dbname" => "blog",
-	 *		"port" => 3306,
-	 *		"username" => "sigma",
-	 *		"password" => "secret"
-	 *	);
-	 *
-	 *	$connection = new Phalcon\Db\Adapter\Pdo\Mysql($config);
-	 *
-	 *</code>
+	 * @param string|array $identifier
+	 * @return string
+	 * @throws Exception
 	 */
-	
-	class Mysql extends \Phalcon\Db\Adapter\Pdo implements \Phalcon\Events\EventsAwareInterface, \Phalcon\Db\AdapterInterface {
+	public function escapeIdentifier($identifier)
+	{
+		if(is_array($identifier) === true) {
+			if(isset($GLOBALS['_PHALCON_DB_ESCAPE_IDENTIFIERS']) === true &&
+				$GLOBALS['_PHALCON_DB_ESCAPE_IDENTIFIERS'] === true) {
+				return '`'.$identifier[0].'`.`'.$identifier[1].'`';
+			} else {
+				return $identifier[0].'.'.$identifier[1];
+			}
+		} elseif(is_string($identifier) === true) {
+			if(isset($GLOBALS['_PHALCON_DB_ESCAPE_IDENTIFIERS']) === true &&
+				$GLOBALS['_PHALCON_DB_ESCAPE_IDENTIFIERS'] === true) {
+				return '`'.$identifier.'`';
+			} else {
+				return $identifier;
+			}
+		} else {
+			throw new Exception('Invalid parameter type.');
+		}
+	}
 
-		protected $_type;
+	/**
+	 * Returns an array of \Phalcon\Db\Column objects describing a table
+	 *
+	 * <code>
+	 * print_r($connection->describeColumns("posts")); ?>
+	 * </code>
+	 *
+	 * @param string $table
+	 * @param string|null $schema
+	 * @return \Phalcon\Db\Column[]
+	 * @throws Exception
+	 */
+	public function describeColumns($table, $schema = null)
+	{
+		if(is_string($table) === false) {
+			throw new Exception('Invalid parameter type.');
+		}
 
-		protected $_dialectType;
+		$dialect = $this->_dialect;
 
-		/**
-		 * Escapes a column/table/schema name
-		 *
-		 * @param string $identifier
-		 * @return string
-		 */
-		public function escapeIdentifier($identifier){ }
+		//Get the SQL to describe a table
+		$sql = $dialect->describeColumns($table, $schema);
 
+		//Get the describe
+		$describe = $this->fetchAll($sql, 3);
+		$old_column = null;
+		$columns = array();
 
-		/**
-		 * Returns an array of \Phalcon\Db\Column objects describing a table
-		 *
-		 * <code>
-		 * print_r($connection->describeColumns("posts")); ?>
-		 * </code>
-		 *
-		 * @param string $table
-		 * @param string $schema
-		 * @return \Phalcon\Db\Column[]
-		 */
-		public function describeColumns($table, $schema=null){ }
+		//Field Indexes: 0 - Name, 1 - Type, 2 - Not Null, 3 - Key, 4 - Default, 5 - Extra
+		foreach($describe as $field) {
+			//By default the bind type is two
+			$definition = array('bindType' => 2);
 
+			//By checking every column type we convert it to a Phalcon\Db\Column
+			$column_type = $field[1];
+
+			//Check the column type to get the current Phalcon type
+			while(true) {
+				//Point are varchars
+				if(strpos($column_type, 'point') !== false) {
+					$definition['type'] = 2;
+					break;
+				}
+
+				//Enum are treated as char
+				if(strpos($column_type, 'enum') !== false) {
+					$definition['type'] = 5;
+					break;
+				}
+
+				//Tinyint(1) is boolean
+				if(strpos($column_type, 'tinyint(1)') !== false) {
+					$definition['type'] = 8;
+					$definition['bindType'] = 5;
+					$column_type = 'boolean';
+					break;
+				}
+
+				//Smallint/Bigint/Integer/Int are int
+				if(strpos($column_type, 'int') !== false) {
+					$definition['type'] = 0;
+					$definition['isNumeric'] = true;
+					$definition['bindType'] = 1;
+					break;
+				}
+
+				//Varchar are varchars
+				if(strpos($column_type, 'varchar') !== false) {
+					$definition['type'] = 2;
+					break;
+				}
+
+				//Special type for datetime
+				if(strpos($column_type, 'datetime') !== false) {
+					$definition['type'] = 4;
+					break;
+				}
+
+				//Decimals are floats
+				if(strpos($column_type, 'decimal') !== false) {
+					$definition['type'] = 3;
+					$definition['isNumeric'] = true;
+					$definition['bindType'] = 32;
+					break;
+				}
+
+				//Chars are chars
+				if(strpos($column_type, 'char') !== false) {
+					$definition['type'] = 5;
+					break;
+				}
+
+				//Date/Datetime are varchars
+				if(strpos($column_type, 'date') !== false) {
+					$definition['type'] = 1;
+					break;
+				}
+
+				//Timestamp as date
+				if(strpos($column_type, 'timstamp') !== false) {
+					$definition['type'] = 1;
+					break;
+				}
+
+				//Text are varchars
+				if(strpos($column_type, 'text') !== false) {
+					$definition['type'] = 6;
+					break;
+				}
+
+				//Floats/Smallfloats/Decimals are float
+				if(strpos($column_type, 'float') !== false) {
+					$definition['type'] = 7;
+					$definition['isNumeric'] = true;
+					$definition['bindType'] = 32;
+					break;
+				}
+
+				//Doubles are floats
+				if(strpos($column_type, 'double') !== false) {
+					$definition['type'] = 9;
+					$definition['isNumeric'] = true;
+					$definition['bindType'] = 32;
+					break;
+				}
+
+				//By default: String
+				$definition['type'] = 2;
+				break;
+			}
+
+			//If the column type has a parentheses we try to get the column size from it
+			if(strpos($column_type, '(') !== false) {
+				$matches = null;
+				$pos = preg_match("#\\(([0-9]++)(?:,\\s*([0-9]++))?\\)#", $column_type, $matches);
+				if($pos == true) {
+					if(isset($matches[1]) === true) {
+						$definition['size'] = $matches[1];
+					}
+
+					if(isset($matches[2]) === true) {
+						$definition['scale'] = $matches[2];
+					}
+				}
+			}
+
+			//Check if the column is unsigned, only MySQL supports this
+			if(strpos($column_type, 'unsigned') !== false) {
+				$definition['unsigned'] = true;
+			}
+
+			//Positions
+			if($old_column != true) {
+				$definition['first'] = true;
+			} else {
+				$definition['after'] = $old_column;
+			}
+
+			//Check if the field is primary key
+			if($field[3] === 'PRI') {
+				$definition['primary'] = true;
+			}
+
+			//Check if the column allows null values
+			if($field[2] == 'NO') {
+				$definition['notNull'] = true;
+			}
+
+			//Check if the column is auto increment
+			if($field[5] === 'auto_increment') {
+				$definition['autoIncrement'] = true;
+			}
+
+			$column = new Column($field[0], $definition);
+			$columns[] = $column;
+			$old_column = $field[0];
+		}
+
+		return $columns;
 	}
 }

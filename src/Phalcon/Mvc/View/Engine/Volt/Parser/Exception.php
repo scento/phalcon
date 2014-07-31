782a10fde020781ba152b2233da0c5c96a8aa0e4
Wenzel Pünter <wenzel@phelix.me>
2014-07-31 18:05:59 +0200

    Volt parser exception implementation

---
+++ src/Phalcon/Mvc/View/Engine/Volt/Parser/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Exception
+ *
+ * @author Wenzel Pünter <wenzel@phelix.me>
+ * @version 1.2.6
+ * @package Phalcon
+*/
+namespace Phalcon\Mvc\View\Engine\Volt\Parser;
+
+/**
+ * Exception
+*/
+class Exception extends \Phalcon\Exception
+{
+
+}

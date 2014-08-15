8195c8f9931aade870d7470745656bab62f4ebdf
Sid Roberts <sid@sidroberts.co.uk>
2014-08-15 07:18:34 +0100

    Fixed a constant typo in \Phalcon\Annotations\Annotation::getExpression().

--- src/Phalcon/Annotations/Annotation.php
+++ src/Phalcon/Annotations/Annotation.php
@@ -175,7 +175,7 @@ class Annotation
 			case self::PHANNOT_T_INTEGER:
 			case self::PHANNOT_T_DOUBLE:
 			case self::PHANNOT_T_STRING:
-			case self::PHANNOT_T_IDENTIFER:
+			case self::PHANNOT_T_IDENTIFIER:
 				return (string)$expr['value'];
 				break;
 			case self::PHANNOT_T_NULL:
